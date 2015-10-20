895aaa10bb55812113fa1bd86b070f0c2c849dce
Halling69 <tha@alpha-solutions.dk>
2015-10-20 22:50:24 +0200

    Loop unrolled solutions perfoming in 61 steps

--- 21-Zero-Terminated-Sum.speed.asm
+++ 21-Zero-Terminated-Sum.speed.asm
@@ -1,51 +1,57 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 
-    COMMENT  0
+    JUMP     h
 a:
-    INBOX   
-    JUMPZ    g
-    COPYTO   0
 b:
-    INBOX   
-    JUMPZ    f
-    ADD      0
-    COPYTO   0
-    INBOX   
-    JUMPZ    e
-    ADD      0
-    COPYTO   0
-    INBOX   
-    JUMPZ    d
-    ADD      0
-    COPYTO   0
-    INBOX   
-    JUMPZ    c
-    ADD      0
-    COPYTO   0
-    JUMP     b
 c:
 d:
 e:
 f:
-    COPYFROM 0
 g:
+    COPYFROM 4
     OUTBOX  
+h:
+    INBOX   
+    JUMPZ    j
+i:
+    COPYTO   4
+    INBOX   
+    JUMPZ    b
+    ADD      4
+    COPYTO   4
+    INBOX   
+    JUMPZ    g
+    ADD      4
+    COPYTO   4
+    INBOX   
+    JUMPZ    d
+    ADD      4
+    COPYTO   4
+    INBOX   
+    JUMPZ    f
+    ADD      4
+    COPYTO   4
+    INBOX   
+    JUMPZ    e
+    ADD      4
+    COPYTO   4
+    INBOX   
+    JUMPZ    c
+    ADD      4
+    COPYTO   4
     JUMP     a
+j:
+k:
+    OUTBOX  
+    INBOX   
+    JUMPZ    k
+    JUMP     i
 
 
-DEFINE COMMENT 0
-eJxzYGBgSFFN0AzTuqWyy+SWyiorNd0nNjusVGxjQtJstOLWWtblFJu7F9cZB1QG6kyuFtHmKJmg41zQ
-YGSd9cXMOivCUbAKaASDhe9i7SL/+RoKYffUY8Lna9yIUNOdFllpfSPC3l4jOMB7WuT0gInR90IFYlKj
-BGJexwVFb0iSiPyckhXEUpgfcLQQZIbx0T6ZPwclVdj2rTHk2cPi7L1HMohtn1acw4Hv6d8OOxfoHT9Z
-8vJEV2nqKY6SpNPOBbJn3yfGnlWMmH/mXqje8ekB/w5c8weZc/V5l+TmO12SOfft7bUeNDk+euTsJPO0
-y/3Gs2v+N549ikh8XJqWc78ux/y2fS7DKBgFIxwAAJqyadQ;
-
-DEFINE LABEL 0
-eJwTZWBguDGfw+3ZrEdakgUxapvT5mt8SK+zy8y6FypZsCJme41zQX+3bPOkroTW1ObnnWdLqqan5PQv
-U8kRXf0oJ3VbWOGCB/JzZe/3Lb515/JCn0uTF+rtZRgFo2AUDBkAANr8KJg;
-
-DEFINE LABEL 5
-eJxTZGBg4OjbYj6lc4t5RpO1DWcZj+eLrMzYtxmFyRvS/2TtSLnXbJViP/tHStfivIwlWx/lfD7wp+rk
-hYymS9eudYjezJ8ecuP2PI4LlxfuPe6zJHdP59KYDezLzKZyLdXqiZxfl5M+Sy26anKXu2/PHxugVQwx
-82TVH89ucvw25WBedmPbuf+VTT8ZRsEoGAUDBgBSuEGi;
+DEFINE LABEL 4
+eJyzZmBgqNAoDfiqvthjvkqkNYeihOlOuQCTt0ISpjf5N9qG8Yf4PhDYFvZW6HvMDMnKzC+yn/LOKspU
+Ple9392qtXVNq9aJXb81Zh7arL7r6C3lWyfWyNw6cUxs5iE94RO7pAXero4R+DQTaAVDhPkKM1kLGb9U
+S//4tVZpG79Zm+90tXuxd5bjxYNbXRMP73abeYjL8/9Oce+pK+94S5iC9OhEfDyiE2G8ICFMpnJ26BO/
+2aG33V+FF5hLxE8Nlk94F5qe+C70R7JaqHuabeDiEgnTVaX6hhvLvhuWlVtYNFe2xzVXxtb/LH85aXHJ
+tfUKJY4HGEbBKBgFGAAAnKFndQ;
 
