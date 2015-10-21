d6ac16708106039921d20151b31e98a18ac49249
Alan De Smet <chaos@highprogrammer.com>
2015-10-21 00:16:54 -0500

    144 cycle solution for 38-Digit-Explorer.speed.asm

--- 38-Digit-Exploder.speed.asm
+++ 38-Digit-Exploder.speed.asm
@@ -1,195 +1,251 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 
-    COMMENT  3
+    COMMENT  1
 a:
     COPYFROM 9
     COPYTO   7
     COPYTO   8
     INBOX   
-    COPYTO   0
-    COMMENT  0
     SUB      11
     JUMPN    j
-    COPYTO   0
     SUB      11
     JUMPN    i
-    COPYTO   0
     SUB      11
     JUMPN    h
-    COPYTO   0
     SUB      11
     JUMPN    g
-    COPYTO   0
     SUB      11
     JUMPN    f
-    COPYTO   0
     SUB      11
     JUMPN    e
-    COPYTO   0
     SUB      11
     JUMPN    d
-    COPYTO   0
     SUB      11
     JUMPN    c
-    COPYTO   0
     SUB      11
     JUMPN    b
     COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     k
 b:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     s
 c:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     r
 d:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     q
 e:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     p
 f:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     o
 g:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    BUMPUP   8
+    JUMP     n
 h:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
+    BUMPUP   8
+    JUMP     m
 i:
+    ADD      11
+    COPYTO   0
     BUMPUP   8
-    OUTBOX  
-    COMMENT  1
+    JUMP     l
 j:
+    ADD      11
+    COPYTO   0
+    JUMP     t
+k:
+l:
+m:
+n:
+o:
+p:
+q:
+r:
+s:
+    OUTBOX  
     COPYFROM 0
+t:
+    SUB      10
+    JUMPN    ac
+    SUB      10
+    JUMPN    ab
+    SUB      10
+    JUMPN    aa
+    SUB      10
+    JUMPN    z
+    SUB      10
+    JUMPN    y
+    SUB      10
+    JUMPN    x
     SUB      10
     JUMPN    w
-    COPYTO   0
     SUB      10
-    JUMPN    r
-    COPYTO   0
+    JUMPN    v
     SUB      10
-    JUMPN    q
-    COPYTO   0
-    SUB      10
-    JUMPN    p
-    COPYTO   0
-    SUB      10
-    JUMPN    o
-    COPYTO   0
-    SUB      10
-    JUMPN    n
-    COPYTO   0
-    SUB      10
-    JUMPN    m
-    COPYTO   0
-    SUB      10
-    JUMPN    l
-    COPYTO   0
-    SUB      10
-    JUMPN    k
+    JUMPN    u
+    COMMENT  0
     COPYTO   0
     BUMPUP   7
-k:
     BUMPUP   7
-l:
     BUMPUP   7
-m:
     BUMPUP   7
-n:
     BUMPUP   7
-o:
     BUMPUP   7
-p:
     BUMPUP   7
-q:
     BUMPUP   7
-r:
     BUMPUP   7
-s:
-    JUMPZ    t
-    JUMP     u
-t:
-    COPYFROM 8
-    JUMPZ    v
-    COPYFROM 7
+    JUMP     ad
 u:
-    OUTBOX  
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     al
 v:
-    COMMENT  2
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     ak
+w:
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     aj
+x:
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     ai
+y:
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     ah
+z:
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     ag
+aa:
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    BUMPUP   7
+    JUMP     af
+ab:
+    ADD      10
+    COPYTO   0
+    BUMPUP   7
+    JUMP     ae
+ac:
+    ADD      10
+    COPYTO   0
+    COPYFROM 8
+    JUMPZ    am
+    COPYFROM 7
+ad:
+ae:
+af:
+ag:
+ah:
+ai:
+aj:
+ak:
+al:
+    OUTBOX  
+am:
     COPYFROM 0
     OUTBOX  
     JUMP     a
-w:
-    COPYFROM 7
-    JUMP     s
 
 
 DEFINE COMMENT 0
-eJxzZWBgULN5rqtmszdbx2p2o46V2nagEENlygaTloTPZj6xf2zmhKdGLQzdm70kRLHtcQjL/PBIpS3+
-MWrbWxJW7NDJlN04N69/2YTiPzPcyp93/q/sKt1b9T5xb1VEIGOlsxPIrLOL1xhKzX9t8GGamSXDRA43
-7t6YkKCuzylXOu81T+raMrmrl2PRnknzN32Yprf32awtB1mXmx/+u/LzgYQtoqtnbK+bJbgroXXCrqOF
-gru04qbukA19uqnUYsVGI7O36zeYNC/fYOK51MgMZM//SmubsMLXBgtyV+ilZWfqf0ivtAaJPz7lXOB/
-6Vpt0dHpddaHZJutDz1uszj8uif3OMOc/ksxG1ivPd7if6ns0M1zdSeizjWdZhgFo2AEAQBMYoF8;
+eJxjYBgFo2AUjGQAAAQEAAE;
 
 DEFINE COMMENT 1
-eJzTZ2BgmBf2XHde2CWfhaFHC5VD+5clhbadAwoz2FWVWoQVTvaNy1OMeJpdmvYii6f8RdbemYm509d4
-lr3fW1xnfexl459jc9qsjwV0v9/r2p+6zXQqxyKD6Z8n5U+PacqfvjOjaNqSyD2T3F2ZJ1Raz2rP1Nds
-WqxtVvtIq7YqRYelIlOfvfy9Mciu4J33mt+uv9d8YEVqB9OKzO7m5Xr99iu3TA7YdW/jqT1au7r2mB/2
-2111NHjnn2Nv1/859nWN8xmGUTAKRgHFAAAjEFtR;
-
-DEFINE COMMENT 2
-eJyTYWBgWF9/0GFRy3vjSV3zNc70hCh49UYoc/c25Z/rlm2+15q5U6H13GWgMoY/Vd0XuXt9Lh2cwHEB
-xG9cMb/FfVlMU9/imKarCxTbpi3YMKFvMcei7iUBy5lW3FpXt1Jte93K3D0HVmzY57PE6MDkheaHGUbB
-KBgFgwoAAIJrNLg;
-
-DEFINE COMMENT 3
-eJzLZmBg+C2jpntYMkXntNhz3Xj+LeapvHV2aryCPnMEMmNPi9nn1ku7F79Qdi++q/4nK1JzQ9INzefR
-UpqXfDKVt5jbS2fqA41guKr12mCa1q2GGZqZO0H8Lrfnuhed9Yzl7Jsc02xkQ9VsOEqSbSUboh2MJvm6
-Cq5o8kzcbOGbuk03cP6mxyGiq2+HH5wjFJ3awRt7tJA3dkXMtMiznqoh7403+i/WrvWWVfd1jVEDmdsX
-k6AZHqllIBVRaNrvUjdrqlPbgvl28zeB5BpXzNdoW+btEbjodZzQosJk0YWyG0HiZ3ZPV5q0M0Fz1rZS
-i9Ub9BLfrq/KfLs+tQPI7pfbyrt00s6Itaf2RKxt3Reyavvho/OLjlZNNzim1VN0VLbZ4vC5cpb99rmL
-Nv/J0tnAkAcyr+bma4OD1/WMW66VWvheFvW/eLEwOegiT3n/pcKJpy83zXW6Pn3NvhsxG3beurdx9X3Z
-jcse3lon9ax/2eQXR+d3vMnsdnknWuvy7nMK15sQvxnPjMwWP8rUf/FghZ4t0FyQ+Y7v52t0v25zkXla
-lfnhrlaP7+XMnRMv/jjMMApGAQ0AAJ0ZvEI;
-
-DEFINE LABEL 0
-eJxzYGBgaDY8V85iaH2MyUj2PgOYv2XyAaPUjldW/RWSjl2l510Eq454PO/c6ROz4aeP85la71t3QOqC
-os+Vh0SVTpkSZX3sQnTbOa64c5cZkkRv6mdcu5mTKXpzWbbPJfHCW+smF5VOmVyU2S1eGFB5tZCnHKRX
-sLitaELx8+08pfYneUoDroLElFsY8ha1uBcrtOrtVWitO6HcInoTJB7bdqlKoqOrVLirrair92ih0bSA
-ytczbjW8mLmk/cXMo/NzZmTuzJlhfezFzGtg9a9naPWkz7rV8Hj25Oq7c7tKJy88mOe4giFv7frJ1a82
-aPW82tC1eO16te1r13dfZBgFo2CEAwB2CXP0;
-
-DEFINE LABEL 7
-eJwLYGBgUFX9MfWHbOmUGhmzqfbSf2a4SnYtdpV8v3ef9J9jX+Wcz6ircFzw0+W4AFTK8MnceoahxclF
-eZYxG/Qs3c+DxPIsneetsqqaHmtfOHG64/v+k24bJjh77pxm4Tt55TdfowMgNROj9RJFo9qKRKPe9wvE
-SK7rjCvcz5S497hN8sFTv5LrTvxIyd0DUvfRn6Nkd7J7MYi9tkHL4EvtksgvtWZTf1bvPAIS+15zS4Wz
-7LXB1MJKaxBfsPjPsY6yH4dDO6Hyq5/rsi5/rjtzfqX1slmXfDZO25C0ZWpT/uep0+sMps9vWTf9dc+D
-Od5LXJZn7rRZlbtnw7r3e+9viVg7dQfDnPO7lrSf37UlVWTn7GCVzZXWW9e8NgCZOWP7PfUdqz+n7F3Z
-VhSykGHO3bn9y1Jmz9+UNfMz2G+jYBQMZQAAGjWSiQ;
-
-DEFINE LABEL 8
-eJyrZQACafvZ9tJV0zvFd067JlQ3K56fY1ESH+9SNd5b6zR4cvdk8BgdkBWwPykq7HymX9Tnkqvk5Ov5
-iiE3QFo99a1ndOi7L+TRcz/frXfpGkjsqAHHogNGdbMMLX5MXWX1Y+od+4NzxJw4FgU7X1sd7Mx7GaTm
-ZVBp2utA+9w1gSdLtIMk76wMkr2fFKr06HJUzL3TsSE3OOLdz4PUcbsltH7zlW2WiJRtBvFXZDE4CBaL
-Kp4vjlELLA5ZNbno3n2QeGH69FvrMiTv3M2TvHOp6NpNw/prN0HiqrPvqTdNkFVv73uk5duTa3Sl097+
-Xuvz6EUtB/MUWs2mXutI3Xam5+Aptv6TFyymCF5bOfPStdnz2s6dXrJh3/8VoqtN1hZOLF7bVbpjtVZc
-83IezxNLd1hdXrhCb+b8R1qJcxI0QXYsOXVLZc3x2apmRxS1HA5sMOna0+Xeu/ua/6Wd88NmbN+ZIbl9
-fovYjjW9k3YaTerYO31N1aEfh9cctz95++zBUz2X7U+2XPtx2Pbmmt2FdwOW599rmqt7f0VXzn2jlMK7
-sqG7bzY5ul4zMhO4lKl/+cJi7eUnZ6uC7Nx8h0Oi8tY1xYPX3xufvHIrqP/S67jQCyyFM8/xlBscm7yy
-Y2/AVZC64rUsopHzteKO9J9c5NF3bfWiFkj4j4JRAAMAAcXkuw;
-
-DEFINE LABEL 9
-eJyTY2Bg0GzSM66u1jNmqTAyW5xz1PlAomzolKiqzGuRDHm3w2c3zgnfOW1aJO/SczExG9KyD566XtB2
-rraq7Zx2o/3Je61lhwS6lbYwT7Cf/W9iTJPtpNI0h4krYs709HnFtu21Xdvw2QxoBYNr/y2Ve60hfsuy
-vZeci7l3n2EUjIJRMCgAAO8uNmE;
-
-DEFINE LABEL 10
-eJwzYGBgUA1hcDgdm6LTGadl0Bkn6u8f41wgEOO+MCTK+QxQmmGL39GzMyPsT+pnHDwF4vd331Of0jlb
-NbklRk278ZHWl9r3xnZVTY7OFZd8zhe3FQkWi9YKFuf28ZcwzOkuDVjuWLlix89qowOltVsOrq+3PynX
-xns5qIv3smt/0+miac+3J87ZOS1+rlpn/NwNSctmifrvnLzGkKPvnjrIrsj505Vez7C3/z1JMii0k6d8
-Ucv8FuUWtU7HytnrJxQ3nWYYBaNgFFAEACCcUMQ;
-
-DEFINE LABEL 11
-eJyLZGBgeKyaoCmuVSdwQWcLp5+uvaCf7l7bS9pacSHauX0h2ombhXSsjwGVMWyWP3jqlwnDKRCbK24H
-j1D0Dp5nwW1i1j7zNU66BXgLO9flhDoFVIY6pXYEOxdOZPPwXlLum7j5ZdD7vcuDtxy8Hb73+K/kuhOf
-0swPv8hS2nK1MKF1QnFT/oRi2VD5vBSdV5neUiVpe/ntk75zn4j7zg2yJ35uIXv+9Pesx/qahCM6QhTk
-2uZrPGxR1HrelGu0vp7Hs6B+fsubhh9TlzZPXjmnTWmLb0/unvIp5oezZpofvjHf6AD7MrXtu1dFrH27
-/uj8uVvVOm9t4ymftc0+9/26Uou2ZTt4Opdu4QxZWMiuNKeQHWRf3FZnkYQt3lIg9oItXZLZGxI0i9dq
-GXguFfWXmq+XeHfu9/THs/dmf54q2xzQzbEotVlpy/eaqqMg9c0VRhxp2XrG7vGlaVIR12qrfJS29LpC
-wmsUjILBCgAmwJPa;
+eJxzYWBgcJXUMpgiLNkAZDKwGyhqXdJ+pDVPfY1himqltaoqh9ti1bOetzSmB1zSjgnx1L8VVGfs7bHX
+uM2lzrjSmsUwUx+kr854sfZ/o89m/41K0w4Z3gKbtT7gmmK572zVPd6Ltdk8nJ3a3ZdE/vNqK7LwFaz6
+6H+t9mXQ9LoHoddqwyPPlQvEHMy7EK0VdzkqNSoqIiF8o/+jCGufzNh6r/eJILN0NkiqrF2/WHvt+ltB
+huvsc0FiXXtCFLr2cLhpn+hyB/GXnxSUW37ytYHe8dQog2M85SAx7ishCr6X/9j0XObxtL8x2df0Togf
+SFz3/mT5nPuLtdfdK7XYfXNnhv2NtiL7GwGVDKNgFIwwAAD5AGcV;
 
