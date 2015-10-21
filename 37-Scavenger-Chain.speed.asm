92f2ee5f019cc4e4ca8cebeea24fe2969e9f4cef
Wolf <wolfwings@wolfwings.us>
2015-10-21 01:33:33 -0600

    Create 37-Scavenger-Chain.speed.asm
    20 cycle solution - Only two possible queries are given by the game, "ESCAPEAPE" or "APEESCAPE" so we can brutally over-optimize the speed version with a simple unrolling.

---
+++ 37-Scavenger-Chain.speed.asm
@@ -0,0 +1,38 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+    INBOX   
+    JUMPZ    a
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 10
+    OUTBOX  
+    COPYFROM 20
+    OUTBOX  
+    COPYFROM 0
+    OUTBOX  
+    COPYFROM 13
+    OUTBOX  
+    COPYFROM 3
+    JUMP     b
+a:
+    COPYFROM 0
+    OUTBOX  
+    COPYFROM 13
+    OUTBOX  
+    COPYFROM 3
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 10
+    OUTBOX  
+    COPYFROM 20
+b:
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 10
+    OUTBOX  
+    COPYFROM 20
+    OUTBOX  
+
+
