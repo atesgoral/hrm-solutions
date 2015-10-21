9be569c6574a2a86e73705f8ef2501e155f2f135
Wolf <wolfwings@wolfwings.us>
2015-10-21 02:24:52 -0600

    Update 40-Prime-Factory.speed.asm
    54 cycle solution using a binary decision tree based on prime numbers, avoiding duplication of code in the output branches as well.

--- 40-Prime-Factory.speed.asm
+++ 40-Prime-Factory.speed.asm
@@ -1,95 +1,188 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
--- Prime Factory - SIZE 48/28 - SPEED 324/399 --
 
--- This solution relies on the fact that you will always get numbers that have single digit prime factors, or are prime themselves. Never seen a number greater than 20 in the inbox. --
--- Basically, it builds a table of 2,3,5,7, then subtracts each of them until it gets zero (which means this is a factor) or -1 (which means it is not). --
--- Then it divides the number by that, replaces the original, and starts subtracting again. --
--- It's finished when the final remaining number is a 1 (which it checks by subtracting 2 from it), or when it hits the zero marker, in which case the number is likely a prime itself. --
-
-    COPYFROM 24
-    COPYTO   4
-    COPYTO   0
-    BUMPUP   0
-    BUMPUP   0
-    COPYTO   1
-    BUMPUP   1
-    COPYTO   2
-    BUMPUP   2
-    BUMPUP   2
-    COPYTO   3
-    BUMPUP   3
-    BUMPUP   3
+    BUMPUP   24
+    COPYTO   23
+    BUMPUP   23
+    COPYTO   22
+    BUMPUP   22
+    ADD      23
+    COPYTO   21
+    ADD      21
+    COPYTO   20
+    BUMPUP   20
 a:
 b:
 c:
-    COPYFROM 24
-    COPYTO   23
-    INBOX   
-    COPYTO   10
 d:
 e:
-    COPYFROM [23]
-    JUMPZ    l
 f:
-    COPYFROM 10
 g:
-    SUB      [23]
-    JUMPN    h
-    JUMPZ    i
-    JUMP     g
 h:
-    BUMPUP   23
-    JUMP     e
 i:
-    COPYFROM [23]
-    OUTBOX  
-    COPYFROM 24
-    COPYTO   11
 j:
-    COPYFROM 10
-    SUB      [23]
-    COPYTO   10
-    JUMPZ    k
-    BUMPUP   11
-    JUMP     j
 k:
-    BUMPUP   11
-    COPYTO   10
-    COPYFROM 10
-    SUB      0
-    JUMPN    c
-    JUMP     f
-    BUMPUP   23
-    JUMP     d
 l:
-    COPYFROM 10
-    JUMPZ    a
+m:
+n:
+o:
+p:
+    INBOX   
+    SUB      20
+    JUMPN    ab
+    SUB      21
+    JUMPN    v
+    SUB      22
+    JUMPN    r
+    SUB      24
+    JUMPN    q
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 21
+    OUTBOX  
+    JUMP     a
+q:
+    COPYFROM 20
+    ADD      20
+    SUB      22
+    OUTBOX  
+    JUMP     p
+r:
+    ADD      24
+    JUMPN    t
+    COPYFROM 23
+    OUTBOX  
+s:
+    COPYFROM 22
+    OUTBOX  
+    COPYFROM 22
+    OUTBOX  
+    JUMP     h
+t:
+    ADD      24
+    JUMPN    u
+    COPYFROM 20
+    ADD      22
+    ADD      22
+    OUTBOX  
+    JUMP     o
+u:
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    JUMP     n
+v:
+    ADD      22
+    JUMPN    y
+    SUB      24
+    JUMPN    x
+    SUB      24
+    JUMPN    w
+    COPYFROM 22
+    OUTBOX  
+    COPYFROM 21
+    OUTBOX  
+    JUMP     m
+w:
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 23
+    ADD      21
+    OUTBOX  
+    JUMP     l
+x:
+    COPYFROM 20
+    ADD      23
+    OUTBOX  
+    JUMP     k
+y:
+    ADD      24
+    JUMPN    aa
+    COPYFROM 23
+    OUTBOX  
+z:
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 22
+    OUTBOX  
+    JUMP     f
+aa:
+    COPYFROM 20
+    OUTBOX  
+    JUMP     j
+ab:
+    ADD      21
+    JUMPN    af
+    SUB      22
+    JUMPN    ac
+    SUB      24
+    JUMPN    s
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 21
+    OUTBOX  
+    JUMP     i
+ac:
+    ADD      24
+    JUMPN    ae
+    COPYFROM 23
+    OUTBOX  
+ad:
+    COPYFROM 23
+    OUTBOX  
+    COPYFROM 23
+    OUTBOX  
+    JUMP     d
+ae:
+    ADD      24
+    JUMPN    z
+    COPYFROM 21
+    ADD      23
+    OUTBOX  
+    JUMP     g
+af:
+    ADD      22
+    JUMPN    ah
+    SUB      24
+    JUMPN    ag
+    SUB      24
+    JUMPN    ad
+    COPYFROM 21
+    OUTBOX  
+    JUMP     e
+ag:
+    COPYFROM 22
+    OUTBOX  
+    JUMP     c
+ah:
+    ADD      22
     OUTBOX  
     JUMP     b
 
 
-DEFINE LABEL 10
-eJyzYWBgCArzCtjueySO21sq9bi7d9ljN5OueI/1M/Z7LV603Vdwxe+AE6uCwtav9op8sPJbdNTSZ4n3
-pyll8DcFZV6qmpc5pTgoMzyHKdErwCp+tU9axGofoJEMeg3zw/KqmSqXVU6bCeJ71TMGv2thDD7V4RXw
-q1MyyK0rOcmo426BXBtTJVdLZ+2/us7aptqI+r6mda1l3bIbQXo05+0IvTuXqXLtHN85P+dE7wCJ3dy9
-tuju4aOFIPbXXWuLBHepNz7ckdphu5llPu+mN/NF15vOZVy7fkbhytp2zlVTimvXHfZOXK/qYbv5vOPR
-naFuDKNgFIwCFAAATcdn3Q;
+DEFINE LABEL 20
+eJzjYGBgMEp/bWCUfjBvd/K5y0Auw5uG1wYZTVtSM5p2HmEgElzrqDtxrrvskEff8+1H+gOWu/Zbz3jY
+0l+R0cRRUlzHUshSwVJIrFmjYBSMAvoBAG1NHvg;
 
-DEFINE LABEL 11
-eJxTYmBg4PZW9eD2Votu91Fv9Pe7vGB9wJKt6wMaDgClGOR9bd0PB5133BcrYb8p2ytgWs6LYL3cNfF6
-uY7LzLLq1z5LdNopl6C1Ky76yuagMNZNID2rGp87fGvMjwhruT8trIXtKEiMazqPp9WMNfET52pMiZz/
-7qjtAu2zgsu7JjxdOyXh5DrJIIZRMApGwYAAAIjXNow;
+DEFINE LABEL 21
+eJwTYmBg0JqZqa87I9eIpSLXiL8kU39C8XvjCcXXav9XepdlNx4tdJjoXLBz8smSbVOu1W6b0jR3zyTZ
+jUf61+wO6tp5JKF17/GphfYnGUbBKBgFQxIAAGlqIpc;
+
+DEFINE LABEL 22
+eJwTZ2Bg2JxWaGqU/tpAK+u5LmNlpv7P6lyj500MDhEds4MjOtSiY9vM0t80nCv/UxXT9KbhXnNyy+ue
+ax0Mc0I7p6+J6FDacqdtxY7nTUYHqqv3Hl+WbX/yQOLOIyfithxkGAWjYBQMegAADh4tmg;
 
 DEFINE LABEL 23
-eJyzZ2Bg0IsysNsUIWHPFpbnej9QMqjenzdmsffdAm7ve83bfetmHYuavuZSvNkGoySzDUDlDFyFRilV
-uQV1j3J8OlflHZ9+K990bltR1+JTpVFLlaqLlhxrYJlf16Ex5VC3TPeh7oK6X51ri7haXqY9qk9Osqta
-E99WtCaeq7A6EWRW0TS1aKsZU9tmzXqw8u7cPVtsFyzZenbx4kVTlwpMdVwhXXFzhV3UyuUXQkBqd26R
-Sg3cylS5c4vjMhB/43a+/K+7phSH7r1UFXn0zXzbY7sXLj26q6/x5N3YsycZg0Fqlt5Y7ZN7yytA8q5k
-0It3kkEgsSfPJINmPPu5m2EUjIJRwAAAbOVx9g;
+eJwTZGBg2JB+L3R15iWfZdneHr0le22rq/fa5jacdHvZeMknuaU0LbVZtLag3nne/8rXuxgr/xz7X8lw
+6mUjy9nQTpaza2Z0X2QYBaNgFAxZAAAROCHS;
 
 DEFINE LABEL 24
-eJzTZ2BgSNZaZWltyePJbL3aR9v+buxku4vp3BbrWqcb3592X3v6msM6fuvz9c02nLYx26DuVb8WqIVB
-L+r49KyEda33Uo4WJiXb5xbFJic5xSQnPYoMz/kXUVOyKeJ1z62YqKX7YjWXr0vWXA7Sk1U8pTipzHfO
-tHq+/EutdlFuXS+CQeLbZn6NeTSJL//fRKZKr0mpHU5THSe3zVg/Y9vMH1OXzn9V2blALe/qgikJH+er
-RZ+cfdhbbqate9/UPFeGUTAKRgHFAADfBFKy;
-
+eJxjY2BgqK7eYPKxLiIwu7Gr9Emz95Lklum3GJDA5Nbbpgc6pznp9xj6/ug7mvGjj3eRZJfNCde2HRcm
+1ZreZKwwvZlfxHbNL3/leZ2cf4e5Mnes5cosnL4nc0bHiSyrJpAZX/Kur9/SYFRwoHNtKMMoGAWjYNAA
+AK/vMXQ;
