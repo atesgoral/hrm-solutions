30515a82ac6ef0e1303f62675f9aeea369685a30
Wolf <wolfwings@wolfwings.us>
2015-10-20 18:48:01 -0600

    163-cycle-score Digit Exploder
    Basic 9-cycle unrolled loop, code stays remarkably readable. Uses the existing 'size' solution digit-output code.
    
    In theory can squeeze off 2 more cycles, but it would double the code-size by unrolling the 'output digit' code with decoders.

---
+++ 38-Digit-Exploder.speed.asm
@@ -0,0 +1,149 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+a:
+    COPYFROM 9
+    COPYTO   7
+    COPYTO   8
+    INBOX   
+    COPYTO   4
+    SUB      11
+    JUMPN    j
+    COPYTO   4
+    SUB      11
+    JUMPN    i
+    COPYTO   4
+    SUB      11
+    JUMPN    h
+    COPYTO   4
+    SUB      11
+    JUMPN    g
+    COPYTO   4
+    SUB      11
+    JUMPN    f
+    COPYTO   4
+    SUB      11
+    JUMPN    e
+    COPYTO   4
+    SUB      11
+    JUMPN    d
+    COPYTO   4
+    SUB      11
+    JUMPN    c
+    COPYTO   4
+    SUB      11
+    JUMPN    b
+    COPYTO   4
+    BUMPUP   8
+b:
+    BUMPUP   8
+c:
+    BUMPUP   8
+d:
+    BUMPUP   8
+e:
+    BUMPUP   8
+f:
+    BUMPUP   8
+g:
+    BUMPUP   8
+h:
+    BUMPUP   8
+i:
+    BUMPUP   8
+j:
+    COPYFROM 4
+    SUB      10
+    JUMPN    s
+    COPYTO   4
+    SUB      10
+    JUMPN    r
+    COPYTO   4
+    SUB      10
+    JUMPN    q
+    COPYTO   4
+    SUB      10
+    JUMPN    p
+    COPYTO   4
+    SUB      10
+    JUMPN    o
+    COPYTO   4
+    SUB      10
+    JUMPN    n
+    COPYTO   4
+    SUB      10
+    JUMPN    m
+    COPYTO   4
+    SUB      10
+    JUMPN    l
+    COPYTO   4
+    SUB      10
+    JUMPN    k
+    COPYTO   4
+    BUMPUP   7
+k:
+    BUMPUP   7
+l:
+    BUMPUP   7
+m:
+    BUMPUP   7
+n:
+    BUMPUP   7
+o:
+    BUMPUP   7
+p:
+    BUMPUP   7
+q:
+    BUMPUP   7
+r:
+    BUMPUP   7
+s:
+    COPYFROM 8
+    JUMPZ    v
+    OUTBOX  
+    COPYFROM 7
+t:
+    OUTBOX  
+u:
+    COPYFROM 4
+    OUTBOX  
+    JUMP     a
+v:
+    COPYFROM 7
+    JUMPZ    u
+    JUMP     t
+
+
+DEFINE LABEL 4
+eJzjZ2BgeNgyXyO1Wc+4oP6o84wCvf7VmbfW2STXnfCPEb0JlGY4Fr/FfEeKt4d44exGxsqj8zOaGE5d
+6VR7ykAAWPhKrivyD1n1OvDPjDWBhRM3+qt1WvvY5zp47cyQdNyZAVLDlLgltSRtwwStrLpZ8nnO8wKL
+WeZ3lNnPZqw0m1pdrdWztVa09kttUz5I7doGhrzlTbLNyi33Nj5skVyn0Nq1+HlT4US5Ntnmi123GjZO
+y+x+PWPLZNXZuXtA6o8um63KunyxNvuys57uy94nuiwPqKxbqdXza9WKHTtWbzn4aW3TaZC6xbOdC0QX
+Hi1kXe5cwLLfLJ3xgFGK3cHCZEL+GwWjYDgAANaHbE4;
+
+DEFINE LABEL 7
+eJwTZ2BguNiVohPUVWpRWmudtTpTduOOFPuTQGGGdRkMDgtypwfkNnAsutahtxcktmaG+WGhRXp7m5ev
+2X19+5rdHnuNDoDEoyKsj330tz42y8HowCZzowMuhhD12MBOn7YiD3f34uuOPOV37Nf0SjoWTgxw+THV
+2bNw4k6fzO7PfpINxv6Tq0v9WAq3+zLkaQdtmLAyqG3BmsBb69YEFu5fH8BwysI34vZx95h7aTb9V1ZY
+d198bt10GmT2gcT5LXVJdbMOJK7YAeJ7xQn67Ejh8QSxrau/W1bUmKXvquYp31prNMm4jnepYX3E2uSW
+kFW/J9nPBqnhLDuYx1JxMG9ps1FKT49Ryrrp39NB4ny7GfIitjPkvdpwssR8zcmS7iXXas8u1upxX/Z5
+0t+V1jMqVzfN3bHafeHuVf3LXJbPXn95Yeo2pTmZO3GFwSgYBYMFAAAprI4v;
+
+DEFINE LABEL 8
+eJwTY2BgMEpvc7kcdTDvn5fsRjGnkxeAQgzCzn1e/S59XrXea+L3Ja3ZrZPpfl4n0+cSSK63xP7k+3r7
+k169R89unHb0LEiMa6n9yaebnM8c2h9wVe94wFWGUTAKRsGQAADOsSVM;
+
+DEFINE LABEL 9
+eJyTZ2BgUGj9brmp7rPZ+eLPZo9y/tiUpHG4/U2aHtAZdzCvLyamSSCmbhZvbP+ylgSlLQxJK3ZkZu09
+fr3A+czRcuczS5u3HPw3kWNR+ZQ/Mz5Me96ZP/1oYdXkNfF8PYI+1tWvDfZXpugArWDQDsp8vjLo8aO/
+SZNXOlfszCiadlaaYRSMglEw4AAAyww8ZQ;
+
+DEFINE LABEL 10
+eJyTZ2BgyMlM0cnJFPT5lHattixVcl1Z6oZ9QGGGFzNLLdj6v1uKdba5yLVN9l3avCRSr+Fo4aa61I5N
+dW0L9Bpmr1/avOWgXJv9yekdR8+29x09WzX54Kn0WWrbry5oW+C59Hmny/KQGscV9rmOK3g8Tyz9bhm4
+KFP/7twYtZpJt1QYRsEoGAUDDgCmkTpv;
+
+DEFINE LABEL 11
+eJzTYWBg0A18baAdpJeoHdS/DMhlOFTx2uBsSa7R9YLPZplZXe6f0pZE/ko+WfIj5XVPXsafGdH5vEtP
+lfYv21vVtdi47sdUw/rXPWsbLlVpN66IAel3rniuu73mvfHW2s9maxtE/UFi9itfGziuyNQPWZipnzin
+1OLDtC73b1MUI35M9i77Mblq+rcpTXNzZnAsipnHsejCorYFjiuqpu9e9b5/89qI+s1rl0SarJ0eYL+S
+xZlhFIyCUUBVAABD5VLj;
