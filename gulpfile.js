b8d07adeef77464b5e3cdc4f5d178499b0c901ce
Ates Goral <ates@magnetiq.com>
2015-10-21 21:58:59 -0400

    Added basic level runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,38 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    tap = require('gulp-tap'),
+    hrm = require('hrm-cpu'),
+    tests = require('hrm-cpu/test/tests.json'),
+    equal = require('deep-equal'),
+    chalk = require('chalk');
 
 gulp.task('default', function () {
-    return gulp.src('*/*.asm');
+    console.log(tests);
+    return gulp.src('*/*.asm')
+        .pipe(tap(function (file) {
+            var tokens = /(\d\d)-([^\/\\]+)(?:\/|\\)(.+\.asm)$/.exec(file.path);
+
+            if (tokens) {
+                var levelNumber = tokens[1],
+                    levelName = tokens[2],
+                    asmFilename = tokens[3];
+
+                var test = tests[levelName];
+
+                if (test) {
+                    console.log(chalk.gray('Running', levelNumber, levelName, asmFilename));
+
+                    var results = hrm(file.contents.toString(), test.inbox, test.floor, true);
+
+                    var outboxMatches = equal(results.outbox, test.outbox);
+
+                    if (!outboxMatches) {
+                        console.error(chalk.red('Output mismatch'));
+                        console.error('  ', 'expected', test.outbox, 'got', results.outbox);
+                    } else {
+                        console.log(chalk.green('OK'));
+                        console.log('  ', 'size', results.size, 'steps', results.steps);
+                    }
+                }
+            }
+        }));
 });
