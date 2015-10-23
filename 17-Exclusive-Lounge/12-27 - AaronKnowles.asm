b86739a88693310a24c14e9cb370edd309cec5c5
AaronKnowles <AaronKnowles@users.noreply.github.com>
2015-10-23 02:30:59 -0400

    Create 12-27 - AaronKnowles.asm
    1 less step with same size

---
+++ 17-Exclusive-Lounge/12-27 - AaronKnowles.asm
@@ -0,0 +1,20 @@
+-- HUMAN RESOURCE MACHINE PROGRAM - SIZE 12/12 - SPEED 27/28 --
+
+    JUMP     d
+a:
+b:
+    COPYFROM 5
+c:
+    OUTBOX  
+d:
+    INBOX   
+    JUMPN    f
+    INBOX   
+    JUMPN    a
+e:
+    COPYFROM 4
+    JUMP     c
+f:
+    INBOX   
+    JUMPN    e
+    JUMP     b
