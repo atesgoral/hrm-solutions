208b4c253e797dcadf13e90df4b4b156aff3b9f3
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 22:55:11 -0500

    Update 7.21.asm

--- 04-Scrambler-Handler-7.21/7.21.asm
+++ 04-Scrambler-Handler-7.21/7.21.asm
@@ -1,8 +1,8 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 -- 04-Scrambler-Handler - SIZE 7/7 - SPEED 21/21 --
--- The copyto command is introduced. Switch each pair of inbox items by copying
--- the first item to memory, taking the second item to the outbox, and then
--- copying the first item from memory to the outbox.
+
+-- The copyto command is introduced and is used to copy the item in hand to
+-- memory.
 
 a:
     INBOX   
