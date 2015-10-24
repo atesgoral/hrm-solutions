c0434c86d84fabcd58fcb9baede8a461295b218e
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 22:59:42 -0500

    Update and rename size.speed.asm to 4.23.asm

---
+++ 07-Zero-Exterminator-4.23/4.23.asm
@@ -0,0 +1,13 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 07-Zero-Exterminator - SIZE 4/4 - SPEED 23/23 --
+
+-- The jump if zero command is introduced. Skip outputting zero's by looping
+-- back if the item in hand is zero.
+
+a:
+b:
+    INBOX   
+    JUMPZ    b
+    OUTBOX  
+    JUMP     a
+
