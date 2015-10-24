ce715c8e0142cc0a9366be5885de07142c921d55
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 23:15:40 -0500

    Undo change

--- 04-Scrambler-Handler-7.21/7.21.asm
+++ 04-Scrambler-Handler-7.21/7.21.asm
@@ -1,7 +1,8 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 -- 04-Scrambler-Handler - SIZE 7/7 - SPEED 21/21 --
 
--- The copyto command is introduced and is used to copy item in hand to memory.
+-- The copyto command is introduced and is used to copy the item in hand to
+-- memory.
 
 a:
     INBOX   
