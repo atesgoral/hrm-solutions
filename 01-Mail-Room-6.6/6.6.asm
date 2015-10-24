b4bdae6f6e44025112117a321ccbc59ad4a99ae9
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 21:49:11 -0500

    Update and rename size.speed.asm to 6.6.asm

---
+++ 01-Mail-Room-6.6/6.6.asm
@@ -0,0 +1,11 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 01-Mail-Room - SIZE 6/6 - SPEED 6/6 --
+-- Straightforward. The first level introduces the inbox and outbox commands.
+
+    INBOX   
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+
