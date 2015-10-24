23d661ea2e718bc8a1e836d036e73f00edac077e
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 23:02:41 -0500

    Update and rename size.speed.asm to 6.24.asm

--- 08-Tripler-Room-6.24/6.24.asm
+++ 08-Tripler-Room-6.24/6.24.asm
@@ -1,4 +1,7 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
+-- 08-Tripler-Room - SIZE 6/6 - SPEED 24/24 --
+
+-- Triple each inbox item by copying to memory and adding it to itself twice.
 
 a:
     INBOX   
@@ -7,5 +10,4 @@ a:
     ADD      0
     OUTBOX  
     JUMP     a
-
 
