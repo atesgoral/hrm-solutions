080e329345dd0ce7e723f2b49047887da3b51abf
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 23:23:42 -0500

    Update and rename speed.exploit-Gimlao.asm to 18.16-Gimlao.asm

--- 09-Zero-Preservation-Initiative-5.25/18.16-Gimlao.asm
+++ 09-Zero-Preservation-Initiative-5.25/18.16-Gimlao.asm
@@ -1,4 +1,7 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
+-- 09-Zero-Preservation-Initiative - SIZE 18/5 - SPEED 16/25 --
+
+-- This solution is level-specific (patterned and quantity-limited input).
 
     INBOX   
     INBOX   
@@ -22,5 +25,4 @@ c:
 d:
     INBOX   
     OUTBOX  
-
 
