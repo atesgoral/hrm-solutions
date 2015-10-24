bbb4cb48fabf059925ae08b82500d4db21558893
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 22:05:25 -0500

    Update and rename size.speed.asm to 6.6.asm

--- 03-Copy-Floor-6.6/6.6.asm
+++ 03-Copy-Floor-6.6/6.6.asm
@@ -1,4 +1,6 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
+-- 03-Copy-Floor - SIZE 6/6 - SPEED 6/6 --
+-- The copyfrom command is introduced. Copy the letters B U G to the outbox.
 
     COPYFROM 4
     OUTBOX  
@@ -6,5 +8,4 @@
     OUTBOX  
     COPYFROM 3
     OUTBOX  
-
 
