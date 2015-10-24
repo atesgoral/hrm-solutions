b004ffd9465d5559202c525c6f459e57c946e83b
nanashi-juanto <nanashi-juanto@outlook.com>
2015-10-23 23:18:40 -0500

    Update 4.23.asm

--- 07-Zero-Exterminator-4.23/4.23.asm
+++ 07-Zero-Exterminator-4.23/4.23.asm
@@ -1,7 +1,7 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 -- 07-Zero-Exterminator - SIZE 4/4 - SPEED 23/23 --
 
--- The jump if zero command is introduced. Skip outputting zero's by looping
+-- The jump if zero command is introduced. Skip outputting zero's by jumping
 -- back if the item in hand is zero.
 
 a:
