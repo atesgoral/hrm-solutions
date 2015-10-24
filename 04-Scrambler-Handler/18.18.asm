5061e9846d564d70ffc2c2b45495b84916a3123f
fd1e4020 <divby0@fd1e4020.com>
2015-10-24 10:45:33 -0500

    Create 18.18.asm
    Unrolled loop three times to match the input size.

---
+++ 04-Scrambler-Handler/18.18.asm
@@ -0,0 +1,22 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+    INBOX   
+    COPYTO   0
+    INBOX   
+    OUTBOX  
+    COPYFROM 0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    INBOX   
+    OUTBOX  
+    COPYFROM 0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    INBOX   
+    OUTBOX  
+    COPYFROM 0
+    OUTBOX  
+
+
