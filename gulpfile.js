a33bc1a2cd13226e018421b1c195e867f3c3c0df
Ates Goral <ates.g@myplanet.com>
2015-10-25 14:30:04 -0400

    Added level name and par validation. Closes #104. Closes #105.

--- gulpfile.js
+++ gulpfile.js
@@ -8,38 +8,56 @@ var gulp = require('gulp'),
 gulp.task('default', function () {
     return gulp.src('*/*.asm')
         .pipe(tap(function (file) {
-            var tokens = /(\d\d)-([^\/\\]+)(?:\/|\\)(.+\.asm)$/.exec(file.path);
+            try {
+                var tokens = /(\d\d)-(.+?)-(\d+)\.(\d+)(?:\/|\\)(.+\.asm)$/.exec(file.path);
 
-            if (tokens) {
-                var levelNumber = parseInt(tokens[1], 10),
+                if (!tokens) {
+                    throw [ 'Invalid path', file.path ];
+                }
+
+                var relPath = tokens[0],
+                    levelNumber = parseInt(tokens[1], 10),
                     levelName = tokens[2],
-                    asmFilename = tokens[3];
+                    sizePar = tokens[3],
+                    speedPar = tokens[4],
+                    asmFilename = tokens[5];
+
+                console.log(chalk.gray(relPath));
 
                 var level = levels.filter(function (level) {
                     return level.number === levelNumber;
                 }).pop();
 
-                if (level) {
-                    console.log(chalk.gray('Running', level.number, level.name, asmFilename));
-
-                    level.expect.forEach(function (expect) {
-                        var results = hrm(file.contents.toString(), expect.inbox, level.floor, true);
-
-                        var outboxMatches = equal(results.outbox, expect.outbox);
-
-                        if (!outboxMatches) {
-                            console.error(chalk.red('Output mismatch'));
-                            console.error('  ', 'expected', expect.outbox, 'got', results.outbox);
-                        } else {
-                            console.log(chalk.green('OK'));
-                            console.log('  ', 'size', results.size, 'steps', results.steps);
-                        }
-                    });
-                } else {
-                    console.error(chalk.red('Level data not found for', levelNumber, levelName));
+                if (!level) {
+                    throw [ 'Level data not found' ];
                 }
-            } else {
-                console.error(chalk.red('Invalid path'));
+
+                if (levelName !== level.name.split(' ').join('-')) {
+                    throw [ 'Level name mismatch' ];
+                }
+
+                if (sizePar !== level.challenge.size.toString()) {
+                    throw [ 'Level size par mismatch '];
+                }
+
+                if (speedPar !== level.challenge.speed.toString()) {
+                    throw [ 'Level speed par mismatch '];
+                }
+
+                level.expect.forEach(function (expect) {
+                    var results = hrm(file.contents.toString(), expect.inbox, level.floor, true);
+
+                    var outboxMatches = equal(results.outbox, expect.outbox);
+
+                    // if (!outboxMatches) {
+                    //     throw [ 'Output mismatch', '(expected [', expect.outbox, '] got [', results.outbox, '])'];
+                    // }
+
+                    console.log(chalk.gray('size', results.size, 'steps', results.steps));
+                });
+            } catch (e) {
+                console.error(chalk.red.apply(null, e));
+                throw 'Validation failed';
             }
         }));
 });
