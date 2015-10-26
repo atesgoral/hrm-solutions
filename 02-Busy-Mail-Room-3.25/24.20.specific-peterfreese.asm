880f9c8b12ed647c0b9533f70f61fb4a14e09748
AaronKnowles <AaronKnowles@users.noreply.github.com>
2015-10-26 02:51:38 -0400

    Update and rename 26.20.specific-peterfreese.asm to 24.20.specific-peterfreese.asm
    Correct program size from 26 to 24

--- 02-Busy-Mail-Room-3.25/24.20.specific-peterfreese.asm
+++ 02-Busy-Mail-Room-3.25/24.20.specific-peterfreese.asm
@@ -1,5 +1,5 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
--- 02-Busy-Mail-Room - SIZE 26/3 - SPEED 20/25 --
+-- 02-Busy-Mail-Room - SIZE 24/3 - SPEED 20/25 --
 
 -- This solution is level-specific (quantity-limited input).
 
