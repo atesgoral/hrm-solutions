dbea86451f31e1f94fd2fef103d937fef08e6725
skwas <skwas@skwas.net>
2015-10-29 15:34:50 +0100

    Improved size 11, and added a size 10 solution for Cumulative Countdown

---
+++ 25-Cumulative-Countdown-12.82/10.95-skwas.asm
@@ -0,0 +1,26 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 25-Cumulative-Countdown - SIZE 10/12 - SPEED 95/82 --
+
+    COMMENT  0
+    JUMP     b
+a:
+    COPYFROM 0
+    OUTBOX  
+b:
+    INBOX   
+    COPYTO   1
+c:
+    COPYTO   0
+    BUMPDN   1
+    JUMPN    a
+    ADD      0
+    JUMP     c
+
+
+DEFINE COMMENT 0
+eJxzY2Bg2O+1waTei6WwybNrMZDLoJLz2awyRdT/WPz7xM64upzTsefKveLmt7gk7Jy2O7lrcVnquaVG
+6bxLVXIY5gQXrehqLfcuO1TxJ+tQhVp0d2mX+/liFmeQOfdzSy1AdMhCPWOLKQx5S5t5l4L4HntZnHn2
+2Nvf2rbDKmHLDqvnG/fabljX5lK5+laQzSqtuL0rv6dXrzqYV7hOsOrVhktVizb3VwTvtM7y3vM8+tSe
+mBDvPZd8endLBk3dcQkof/QsyMyD1w86uF+dHiB80Shl+nnngmvnL1UJXIppOnZ1SfuHu7l9uvfrZq27
+576w8O70NaZ35m+qvJW6zf7G8+3uV1O3CVxS2iJx/tY6kDkMN446n3iz15ZhFIyCEQIAlyWDHA;
+
