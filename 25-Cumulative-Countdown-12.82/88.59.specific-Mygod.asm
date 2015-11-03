a7ecf6ef8f85a25f06caaa04343f799a1fdb55fa
Mygod <mygodstudio@gmail.com>
2015-11-03 23:19:01 +0800

    Speed 59 for year 25

---
+++ 25-Cumulative-Countdown-12.82/88.59.specific-Mygod.asm
@@ -0,0 +1,107 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 25-Cumulative-Countdown - SIZE 88/12 - SPEED 59/82 --
+
+-- This solution is level-specific (patterned and quantity-limited input).
+-- Pattern: [34][1-9]0[1-9]
+
+    INBOX   
+    COPYTO   0
+    COPYTO   1
+    BUMPDN   0
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    ADD      1
+    OUTBOX  
+    INBOX   
+    JUMPZ    a
+    COPYTO   0
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    a
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    ADD      1
+    COPYTO   1
+a:
+    COPYFROM 1
+    OUTBOX  
+    INBOX
+    OUTBOX
+    INBOX   
+    COPYTO   0
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+    BUMPDN   0
+    JUMPZ    b
+    ADD      1
+    COPYTO   1
+b:
+    COPYFROM 1
+    OUTBOX  
+
+
+DEFINE LABEL 0
+eJxjZ2BguGGSkXTDZHvXIaPzBw4ZdZ0FCjHEWSpqMoyCUTAKhj0AABHoCdo;
+
+DEFINE LABEL 1
+eJzTY2BgqNOZXz5FbXrqdZX2lANK7Smu8tvSC6WNCgqlvZr1ZUUmblXMWhGjyLq5XTT79D6h6FNALQzT
+zOaX25o92/DebOKeheb3D7Jayh5b6xB96rCTzQk7tw37EjxXrDnidW9etldBCUj9zLDs032hG/b1hX7t
+cwiTbhCJepJ1MXpZ2ue4L9miScs7FZKjT+UlTdwzO+3vAt90kYlcmdqNrTnK+ZEFVzP3Fn7Jnlha0Tan
+7NzczRUm2zZXbNjHMApGwSigCgAAEp9R0Q;
+
--- README.md
+++ README.md
@@ -54,7 +54,7 @@ To import one of these solutions into the game:
 | 22 | Fibonacci Visitor | [16/19](22-Fibonacci-Visitor-19.156/16.126-skwasjer.asm) by [@skwasjer](https://github.com/skwasjer) | [ 51/156](22-Fibonacci-Visitor-19.156/149.51.specific-Mygod.asm) by [@Mygod](https://github.com/Mygod) |
 | 23 | The Littlest Number | [12/13](23-The-Littlest-Number-13.75/12.71-peterfreese.asm) by [@peterfreese](https://github.com/peterfreese) | [32/75](23-The-Littlest-Number-13.75/39.32.specific-Mygod.asm) by [@Mygod](https://github.com/Mygod) |
 | 24 | Mod Module | [11/12](24-Mod-Module-12.57/11.53-nanashi-juanto.asm) by [@nanashi-juanto](https://github.com/nanashi-juanto) | [29/57](24-Mod-Module-12.57/35.29.specific-Mygod.asm) by [@Mygod](https://github.com/Mygod) |
-| 25 | Cumulative Countdown | [10/12](25-Cumulative-Countdown-12.82/10.95-skwasjer.asm) by [@skwasjer](https://github.com/skwasjer) | [77/82](25-Cumulative-Countdown-12.82/77.68.specific-Mygod.asm) by [@Mygod](https://github.com/Mygod) |
+| 25 | Cumulative Countdown | [10/12](25-Cumulative-Countdown-12.82/10.95-skwasjer.asm) by [@skwasjer](https://github.com/skwasjer) | [59/82](25-Cumulative-Countdown-12.82/88.59.specific-Mygod.asm) by [@Mygod](https://github.com/Mygod) |
 | 26 | Small Divide | [15/15](26-Small-Divide-15.76/15.71-ekx.asm) by [@ekx](https://github.com/ekx) | [47/76](26-Small-Divide-15.76/60.47.specific-Mygod.asm) by [@Mygod](https://github.com/Mygod) |
 | _27_ | _Midnight Petroleum_ | &ndash; | &ndash; |
 | 28 | Three Sort | [32/34](28-Three-Sort-34.78/32.128-albertferras.asm) by [@albertferras](https://github.com/albertferras) | [73/78](28-Three-Sort-34.78/62.73-AlanDeSmet.asm) by [@AlanDeSmet](https://github.com/AlanDeSmet) |
