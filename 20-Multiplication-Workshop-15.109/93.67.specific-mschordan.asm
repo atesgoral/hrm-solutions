eb82745a01f27a40f39cb7095e282a2a34939aee
nanashi-juanto <nanashi-juanto@outlook.com>
2015-11-06 03:09:36 -0600

    Adjust for #151

---
+++ 20-Multiplication-Workshop-15.109/93.67.specific-mschordan.asm
@@ -0,0 +1,119 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 20-Multiplication-Workshop - SIZE 93/15 - SPEED 67/109 --
+
+-- This solution is level-specific (quality-limited input) and assumes input
+-- values of 0-9.
+-- Based on 161.70.specific-mschordan.asm
+
+    BUMPUP   9
+    BUMPUP   9
+
+    INBOX   
+    JUMPZ    out1
+    COPYTO   0
+    INBOX   
+    JUMPZ    out
+
+    SUB      9
+    JUMPN    b1
+    JUMPZ    b2
+    SUB      9
+    JUMPN    b3
+    JUMPZ    b4
+    SUB      9
+    JUMPN    b5
+    JUMPZ    b6
+    SUB      9
+    JUMPN    b7
+    JUMPZ    b8
+
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    COPYTO   3
+    ADD      3
+    ADD      3
+out:
+    OUTBOX
+a:
+    INBOX   
+    JUMPZ    out1
+    COPYTO   0
+    INBOX   
+    JUMPZ    out
+
+    SUB      9
+    JUMPN    b1
+    JUMPZ    b2
+    SUB      9
+    JUMPN    b3
+    JUMPZ    b4
+    SUB      9
+    JUMPN    b5
+    JUMPZ    b6
+    SUB      9
+    JUMPN    b7
+    JUMPZ    b8
+
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    COPYTO   3
+    ADD      3
+    ADD      3
+    JUMP     out
+
+out1:
+    OUTBOX  
+    INBOX   
+    JUMP     a
+
+b1:
+    COPYFROM 0
+    JUMP     out
+b2:
+    COPYFROM 0
+    ADD      0
+    JUMP     out
+b3:
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    JUMP     out
+b4:
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    ADD      0
+    JUMP     out
+b5:
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    ADD      0
+    ADD      0
+    JUMP     out
+b6:
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    COPYTO   3
+    ADD      3
+    JUMP     out
+b7:
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    COPYTO   3
+    ADD      3
+    ADD      0
+    JUMP     out
+b8:
+    COPYFROM 0
+    ADD      0
+    ADD      0
+    ADD      0
+    COPYTO   3
+    ADD      3
+    JUMP     out
+
