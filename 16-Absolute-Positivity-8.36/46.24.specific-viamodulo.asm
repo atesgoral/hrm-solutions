75fa9dae3d17f7348c17d7b4b58d1da98b50a1c3
viamodulo <aurelienhmarion@gmail.com>
2015-11-14 19:24:53 +0100

    Speed 24 for year 16

---
+++ 16-Absolute-Positivity-8.36/46.24.specific-viamodulo.asm
@@ -0,0 +1,54 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 16-Absolute-Positivity - SIZE 46/8 - SPEED 24/36 --
+
+-- This solution is level-specific (patterned and quantity-limited input).
+-- The input follows the pattern: +-?0-?+
+
+    INBOX   
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    SUB      0
+    SUB      0
+    OUTBOX  
+    INBOX   
+    JUMPN    a
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    SUB      0
+    SUB      0
+    OUTBOX  
+    INBOX   
+    JUMPN    c
+    OUTBOX  
+    INBOX   
+    OUTBOX 
+    INBOX
+a:
+    COPYTO   0
+    SUB      0
+    SUB      0
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    SUB      0
+    SUB      0
+    OUTBOX  
+    INBOX   
+    JUMPN    c
+    OUTBOX  
+    INBOX   
+    OUTBOX 
+    INBOX
+c:
+    COPYTO   0
+    SUB      0
+    SUB      0
+    OUTBOX  
+    INBOX   
+    OUTBOX  
