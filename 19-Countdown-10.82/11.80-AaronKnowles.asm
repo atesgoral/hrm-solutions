7d8396574487b2d813093fc6f017eb059f63802c
Ates Goral <ates.g@myplanet.com>
2015-11-15 20:10:22 -0500

    Removed dud comment

--- 19-Countdown-10.82/11.80-AaronKnowles.asm
+++ 19-Countdown-10.82/11.80-AaronKnowles.asm
@@ -1,21 +1,20 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
 -- 19-Countdown - SIZE 11/10 - SPEED 80/82 --
 
-    COMMENT  0
 a:
-    INBOX   
+    INBOX
     COPYTO   0
     JUMPN    d
 b:
 c:
-    OUTBOX  
+    OUTBOX
     BUMPDN   0
     JUMPN    a
     JUMP     b
 d:
 e:
-    OUTBOX  
+    OUTBOX
     BUMPUP   0
     JUMPN    e
     JUMP     c
-    
+
