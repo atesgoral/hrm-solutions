89d13f0e49401b44b89503470c8ad85c7e5f7d8f
David King <david.alexander.king@queensu.ca>
2015-11-15 19:03:45 -0500

    Use location in code to maintain to record which unique inputs have been input
    simplify logic by reducing conditionals based on knowledge of which unique inputs have been encountered
    
    use fallthroughs to avoid jump instructions

--- 32-Inventory-Report.speed.asm
+++ 32-Inventory-Report.speed.asm
@@ -5,77 +5,183 @@
     COPYFROM 14
     COPYTO   19
     BUMPUP   19
-a:
-b:
+    ADD      19
+    COPYTO   18
+    COMMENT  0
     INBOX   
     SUB      4
-    JUMPZ    d
-    JUMPN    c
+    JUMPN    f
+    JUMPZ    c
     COPYFROM 19
     OUTBOX  
-    JUMP     e
-c:
-    ADD      19
-    ADD      19
-    OUTBOX  
-    JUMP     b
-d:
+    COMMENT  3
+a:
+    INBOX   
+    SUB      4
+    JUMPN    b
     COPYFROM 14
     OUTBOX  
-    JUMP     j
+    COMMENT  4
+    INBOX   
+    SUB      4
+    ADD      18
+    OUTBOX  
+    INBOX   
+    SUB      4
+    ADD      18
+    OUTBOX  
+    INBOX   
+b:
+    ADD      18
+    OUTBOX  
     JUMP     a
-e:
-f:
-g:
-    COMMENT  1
-    INBOX   
-    SUB      3
-    JUMPN    h
-    JUMPZ    i
+c:
     COPYFROM 14
     OUTBOX  
-    JUMP     g
-h:
-i:
-    ADD      14
-    ADD      19
-    OUTBOX  
-    JUMP     f
-    COMMENT  0
-j:
-k:
-l:
+    COMMENT  1
+d:
     INBOX   
-    SUB      3
-    JUMPN    n
-    JUMPZ    m
+    SUB      4
+    JUMPN    e
     COPYFROM 19
     OUTBOX  
-    JUMP     l
-m:
-n:
-    ADD      14
-    ADD      19
+    COMMENT  2
+    INBOX   
+    SUB      4
+    ADD      18
     OUTBOX  
-    JUMP     k
+    INBOX   
+    SUB      4
+    ADD      18
+    OUTBOX  
+    INBOX   
+e:
+    ADD      18
+    OUTBOX  
+    JUMP     d
+f:
+    ADD      18
+    OUTBOX  
+    COMMENT  5
+    INBOX   
+    SUB      4
+    JUMPN    j
+    JUMPZ    h
+    COPYFROM 19
+    OUTBOX  
+    COMMENT  7
+    INBOX   
+    SUB      4
+    JUMPN    g
+    COPYFROM 14
+    OUTBOX  
+    INBOX   
+    SUB      4
+g:
+    ADD      18
+    OUTBOX  
+    INBOX   
+h:
+    COPYFROM 14
+    OUTBOX  
+    COMMENT  8
+    INBOX   
+    SUB      4
+    JUMPN    i
+    COPYFROM 19
+    OUTBOX  
+    INBOX   
+    SUB      4
+i:
+    ADD      18
+    OUTBOX  
+    INBOX   
+    COPYFROM 19
+    OUTBOX  
+    INBOX   
+j:
+    ADD      18
+    OUTBOX  
+    COMMENT  6
+    INBOX   
+    SUB      4
+    JUMPZ    k
+    COPYFROM 19
+    OUTBOX  
+    INBOX   
+k:
+    COPYFROM 14
+    OUTBOX  
+    INBOX   
+    COPYFROM 19
+    OUTBOX  
 
 
 DEFINE COMMENT 0
-eJyzYWBgWMLLe5lBOrN7s3xdzgvlLvfHqpXW8upNjlKaihEn9HP3uBg6n7E0XfIYqJThl8n0WyusV3Qp
-2ilGRDvU2YHE7oWxnH0cMn9TagjL/NSQydXKoTszYsKv+UtEnnQ7F1Nn15JQaW2eam2TmdXkGJfH43m2
-RC3aueJg3v/K6XX7K82mNlccnMNTemlFZIHsxqfZqdsaE9fsBpkbErX3uE3yhn0gNvP1+RqnL89WnXlu
-tqrBMS2DCbsqreduZXHO3Cjqr78+M9ZwnXtx4TrZ5tUbtkyW2XZt9aWdqdtY9hfuLzi29/ics85nvK64
-n1d/2H2RYRSMglGAAgD0eGZT;
+eJyTYWBguKXhfn6G5paDfrpNcy1NteIKLOztV1gfFIq0YQCDblsGBmFnxTYPd6MDrR72J0FidcalU1o9
+6maB2D6xgnIMSXU5NsmyG38l/zlmnsp7GSQuWCyp4lwxPcC6mqUwo2nnkXNt//8zjIJRMAoGFQAAVe0k
+mQ;
 
 DEFINE COMMENT 1
-eJwzYWBgMOCcfXcJ797jjVLXanfKfk4xUhD0WadYaJqpXMj+QtnMUklNq+eG5t7jfbpKj4DKGUA0v+7O
-Iz56r3scjUT9j1gwgMF5lyWPLzr7XBJ0aVvg7ZbQetDze/q7ACOz5cE80qkh7uJJoSclZkao6V6OqrPz
-j4kJ4Y1lKfSPed0jFM2x6EaE0pZ5YYX7l4RUHdUOqjsxydn5DMjM7TWvDSymuBe7LF+xA8TPW29kVrzW
-2sZ92fPoyPknSz5Pze2b1NW/zLgud09a9rWbDKNgFIwCkgAAjzxSZg;
+eJwzZmBgOCeq95pV8s+xbXI/pq5WMktPV6mze6za5e6jVzjxgNHBU5amjx9tNZu8Ms+yLkfFdoUeUAtD
+uW/3RUavySuPeCS0HvG4VNXk2VZU78VSKBUhWNUXI9nQkpDaYZ+U22eeenR+Wers9TXJmTvb4s0PC0Uz
+nIoPO3r2daD7+Y/+TadBZhWuK7WwXL3B5PSSDSbxc79brplx0KG/e028WrN32c9qrR63cud53qWiq71L
+EzdzlmntYqnYcrC2yv5kduO5y8Jdojfb+67dFF/Qdo5hFIyCUUAyAAA8SFqT;
 
-DEFINE LABEL 3
-eJyTYGBg2O7r7rrR/1LV8uDn24FcBgcvI7M93loGH/0VtdKDY9TmhcWoHUjMNdqWetR5W6q3x6/kEL/l
-wTszTsd+T3dJqMqsTDmYZ57KUliW2l9hnipa6x7/Y2pM+LXVGsFKWxhGwSgYBYMaAADd8ia6;
+DEFINE COMMENT 2
+eJxzZWBg+MJ+6dpNwf4Kf9EYtZXK7ueXqew8oqRWN2uG5t7sq1q3gu7pMoDBfDv7k8m2Pw5/Mq+aXm1y
+sqTB6FFEs2GCJpPRJbk6YxbRVVaT5X1dC02ZPY867/GeHfzN91LVdt8VXTt9dk6r9zq31Nlz8srj7tPX
+CLrcWgcyr6JGTfdP1SMt5wo13e7SXKPZ+U2OOZkxIRvSteI+pX1P35wmWrsu4/OkFVlNc+/mRawVL5y/
+iaVCa9f6+sydS5uXbAWZMXP+5JXhC66tDlm4ZKvowg37IufXnUia43NJd4bknd+T1J7y9bx/DVJXtsZb
+Knrbe+Mj+y75rHxgfhjsoRuX5A5fE1X0v1RqoX3Cvbhjb+4ev92XrjGMglEwggAAyeJ7wQ;
+
+DEFINE COMMENT 3
+eJzTZmBgUOAvfHtMom5Wibyof4l8atQHha7SxaqJm++q670W15p83c0gt2+XSZMjUCmDg9f71x7uDKc4
+3ffOZPOQbd7vlZtQ7ivqv9Hf2+Nl0FnPGxExIU6Jf7J+pJwrX535edKLrPmbtLKMDnxKazsnEPP86Y2I
+1y+eBb9+sdF/yWOQedy93h4bpwVUxszrX3ZiKctZkJjjCjPLs4vXxEvNF6zKmfF50qrGiNtfao3eMYyC
+UTAKqA4ArTJM/Q;
+
+DEFINE COMMENT 4
+eJyzZWBg6BFb89JLnPfyZ3nBqnSVFJ1EtZMS3XqFE5sNU7dtNXv9YpFtiN8j21sqQKUMuYFqT8t9p9/a
+481xwdnz3kZmzw0TDnqqde73Smjd6SPb/C5gTe+c8P5lXHHWx9zjeS+fiAu5MTFa8s6NCMk7K4NCboDM
+uF6gpmtcx1FSM+naTRD/38QUnYMT3hsLdDsX3GxP7UhuOTinueLc5eNlSx6D5BsOMJx6tHnDGxB71fcY
+tQPvJVVmPLul8uCxrPqHu1oGDDecnXgvP4+eeNE+V/iiZEPQxaPzO6883vL75t7j6+5xXFB/6HNJ/Pm5
+y3Yfui9u+MpylmEUjIJRAAcAGSxx6A;
+
+DEFINE COMMENT 5
+eJyLYmBg6BSffP20WNNpbvEVO2xlNiT9luHx3C1zVrpS1l5wm1yTsJGCv2yi2qMIEe01vUI69rMn6Gw5
+eFa3bUGbgXfZVrNSi2JzFlGgMQzCzgdPzXKIWCvtUDfrjr1eYrTDNf9gZ3dxb7fv3LXea5hN/V4zLQzt
+kpSKmK16LVLPWDTqoMPMiBUxWUGpHaV+8zft9yrcf9LN+hjIrGXZ55ZeLRSs8itRi+4ok1V3rrAX/F/5
+ntWw/pKcZpOesXBXRD13b8BykFrOsraitQ2CVQ9beMrBemcxnFKdvXfmgzl/sgIXnZQ4vYQBDF5tUNSa
+u3VJpNgOxbYrO2avB4ltWHdWWn89h8SrDU3C97fs4HHb95715Ql7waTT7uKyZyfLx55dYzj/TKnFqhNH
+nSsOX/IR3HXN/96Z6QFBF2NC2q8+j268Xphse7Mu5/fNS1W2N593Hr5mPaPzStuCiRf7l60/lrhZbEfu
+nkWbc/cwjIJRMEgBAMggmPw;
+
+DEFINE COMMENT 6
+eJwLZWBguChy606P2J8ZPWIMDhwSZ6UZpFN07qoXTvTUrzrqYnjt5i+T94lrLRnA4KQbx4UzrhFrT7rd
+avjntdf2p0+KznbfBM31AYu1XwZpGUhENjmei5ke4JS4N7su6Vz5vqTZjfuSVnQ5JdrPPhE3eWVQ9JKt
+D0K3HDzuXnYo2NnoAMjMq4UMpzrKGOYcLV/SzljpXPCnKkGzoqZNbH19io5wF8einp6m09y9Cx6A1G6t
+lVy3qCVmg0TH7PUgPtu+P8d49rifX7BF8s7ZxUbvQGIFxwpN1U7dC114miFP+pze3unnI26DxH8eeqS1
+5chs1dzj05UELgnKNV6/JFd4V1Zd6lmfl/jziMBpz7XiIp9tSHrw2Cx95QPrrMbrB/MuX2DIM7/dlL/y
+gXfZ3SezG288y+wOeWE9I+RFwPJpz2+tk3qWum3lg7JDP27VnYg923Ta4nDTaYZRMAqGEAAAJaqfHA;
+
+DEFINE COMMENT 7
+eJyLZ2BgOMK44tk3tlvrvrA3zf3IoRWXxX1JbjnPdKXbAizOrpJtC2xl1LYbKcy+u1FhRZeRAkeJrUyf
+TI0MgxBQK4ObQcBVNwPrYyLaVZlhWpXWk7Xna5zVnSz/x9hecIdpk3Ce5XSlV1YpOmo2ZpZpNizOajYx
+Ia+szNLLzF731BnHbDhg9OeYnXHEbZBZJ93KDp1x1dp10fnPjH6XydXebooRPP4MYOAaxMCwJIRB6EL0
+mvgTcV2LWxL09oLEz7iK1rZ6XKra7nupyj+mqxQk9inN+tjdvC2TBYszY1lKIPqfN3VJJrRm6t9puxc6
+p63qKEhMur2St2gai+iLmScl7s4NURBfoKgltMjZSXSht4fU/Mm+66bLhk5e+DmlewlDXufS/gqupfNb
+YuYdnPNiJu9SiynT10zveL4dHAb7bq0T23Hrzp2tmc9B/KTTXZKiFxZru14L8K65qZe47OG11Q8eQ9TO
+e7JYm/Va4cQpF7YcZBgFo2AQAAADJpW+;
+
+DEFINE COMMENT 8
+eJyLZWBgmCK8YscU4XsbrWR5PD/LHxTKV+SRVlfZYRWnfq32qlbXYj/dx1uaDQv3W5ryXi4zcz+vad21
+eKmNWucd+8nyQO0M6wMyd1r4Clb99IkILPVL0MwP6JNRDbHiSwrdwSMbbi94LZJH+nRspn5Lwkm3uqTC
+5LqkydUtCbl9PrFH54tGhay6FzZ7Pcgcz7IlW9nLm+bWVp0r/1m9Jv5LLY90Xj0DGJQA6aXNJyUmdd0K
+cu1f0m47yfoYSNywXq2zacKaXhBba+be40pzVnTJzw3xi59byQsS61xaavF35bnyrWtiNoD4/otTdPau
+TNBcsCVBM3jnBpOuPSzOx/de8jm+d3rAqT0xIYs2v46bsKspn22faC3L/nvNh/Zbzzi+l2OR3+7+Zfe3
+RKx9ux7i1pcnMnfaHWw6zbn32k0Qf9OXGDWWd7Lql16sMZR/4u6a+cAsPfMBR4n6w0tVIS+a5ra9lVy3
+94PSFrPPj7cwjIJRMIgAAHKDnfg;
 
 DEFINE LABEL 4
 eJyTZGBgeDEzU//d9MXaDBNj1BJaJVU4y2LUXmU+13WP32ElGnXU+V3AksgtfqVp233bisp9JRu++W6Y
@@ -88,6 +194,12 @@ M3hjvcu44iLWHou3P1mXZH1MK2v2+gnFK7o4yyLqO8rOlYP0MkxkOXukn+VsV2/diekd9zbGtrHMT2h9
 3ZPQOr1uThtLYWjn9/SenveJBydkxppOfR79YE5m7I35G5IuLPqTxb7sUtWBFWt67Vcenc+0Qnaj59LC
 /ZcX7j0eP9f5TNbMo2erJjufYcACWJbN2fxheet2j7XJe7M2bdh3dSvnPsedx3c93nN/G8uh06unHumZ
 yXz0brfY4TdVq/ZPy+ncdSHp6tbncdjMGgWjYLgDAA6Ce0Y;
+
+DEFINE LABEL 18
+eJxTYmBgeBbMI73H+6TEEY8uST5XQbloh9mqr6zeG6+yanKUsxf11w76kxUf5l7sH7Ok3SfWaNK5mD8z
+LkcdnX87/NzS14Giq4+7X1v93mLySqBRDNH5DHlhhUu2gtj6GZXWIDq2TTGiefn8Ta821J0A8TuXbkmN
+nJ/Z/WKm95IzPecuC3TH3GPAAm62F+6Xbt95RLr94KnQzsnXvXrVnv6YXPYhfVbdN/7FB3+YrGX4EbbD
+/OOzk7L3558RvalwRvAaNnNGwSgYBbgBAEp/VaQ;
 
 DEFINE LABEL 19
 eJyLYGBgCBVmEAoS8ZZql2iae1Cq7gRQiOEjh3PBPX7ngh+yJ0tiNLpKQWJndesEzuqyiDYbbjG3NDVK
