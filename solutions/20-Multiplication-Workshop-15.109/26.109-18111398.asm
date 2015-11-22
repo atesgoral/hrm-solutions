e2ed120c8a1c440acdba2850ac2f3c1b76dddc68
18111398 <18111398@qq.com>
2015-11-22 12:18:51 +0800

    Speed 109 for Year 20

---
+++ solutions/20-Multiplication-Workshop-15.109/26.109-18111398.asm
@@ -0,0 +1,39 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 20-Multiplication-Workshop - SIZE 26/15 - SPEED 109/109 --
+
+    JUMP     d
+a:
+    INBOX   
+    COPYFROM 9
+b:
+c:
+    OUTBOX  
+d:
+    INBOX   
+    JUMPZ    a
+    COPYTO   0
+    COPYTO   1
+    INBOX   
+    JUMPZ    b
+    SUB      0
+    JUMPN    e
+    ADD      0
+    COPYTO   1
+    JUMP     f
+e:
+    ADD      0
+    COPYTO   0
+f:
+    COPYFROM 1
+g:
+    COPYTO   2
+    BUMPDN   0
+    JUMPZ    h
+    COPYFROM 1
+    ADD      2
+    JUMP     g
+h:
+    COPYFROM 2
+    JUMP     c
+
+
