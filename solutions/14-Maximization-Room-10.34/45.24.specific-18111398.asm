e6f1a4a444be8101948207a015c14dc6975be955
18111398 <18111398@qq.com>
2015-11-22 15:37:01 +0800

    Speed 24 for Year 14

---
+++ solutions/14-Maximization-Room-10.34/45.24.specific-18111398.asm
@@ -0,0 +1,59 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 14-Maximization-Room - SIZE 45/10 - SPEED 24/34 --
+
+-- This solution is level-specific (quantity-limited input).
+
+
+
+    INBOX   
+    COPYTO   0
+    INBOX   
+    SUB      0
+    JUMPN    a
+    ADD      0
+    JUMP     b
+a:
+    COPYFROM 0
+b:
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    INBOX   
+    SUB      0
+    JUMPN    d
+    ADD      0
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    INBOX   
+    COPYTO   0
+    INBOX   
+    SUB      0
+    JUMPN    c
+    ADD      0
+    OUTBOX  
+    INBOX   
+c:
+    COPYFROM 0
+    OUTBOX  
+    INBOX   
+d:
+    COPYFROM 0
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    INBOX   
+    COPYTO   0
+    INBOX   
+    SUB      0
+    JUMPN    e
+    ADD      0
+    OUTBOX  
+    INBOX   
+e:
+    COPYFROM 0
+    OUTBOX  
+
+
