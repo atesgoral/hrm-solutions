9e0af0512f1f935a766774c6d3a3862749425c41
18111398 <18111398@qq.com>
2015-12-04 23:45:24 +0800

    Size 7 for Year 16

---
+++ solutions/16-Absolute-Positivity-8.36/7.61-18111398.asm
@@ -0,0 +1,14 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 16-Absolute-Positivity - SIZE 7/8 - SPEED 61/36 --
+
+a:
+    INBOX   
+b:
+    COPYTO   0
+    SUB      0
+    SUB      0
+    JUMPN    b
+    OUTBOX  
+    JUMP     a
+
+
