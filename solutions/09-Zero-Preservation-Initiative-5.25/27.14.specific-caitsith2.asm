d8334e1c042d876ae73a8a663c102430a6913265
CaitSith2 <CaitSith2@caitsith2.com>
2015-12-14 14:27:47 -0800

    Create 27.14.specific-caitsith2.asm

---
+++ solutions/09-Zero-Preservation-Initiative-5.25/27.14.specific-caitsith2.asm
@@ -0,0 +1,35 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 09-Zero-Preservation-Initiative - SIZE 29/5 - SPEED 14/25 --
+
+-- This solution is level-specific (patterned and quantity-limited input).
+-- Based on 29.14.specific-viamodulo.asm
+
+    INBOX   
+    INBOX   
+    OUTBOX  
+    INBOX   
+    JUMPZ    a
+    INBOX   
+    INBOX   
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    JUMPZ    c
+    INBOX   
+    OUTBOX  
+    INBOX   
+a:
+    OUTBOX  
+    INBOX   
+    INBOX   
+    OUTBOX  
+    INBOX   
+    OUTBOX  
+    INBOX   
+    JUMPZ    c
+    INBOX   
+c:
+    OUTBOX  
+    INBOX   
+    OUTBOX  
