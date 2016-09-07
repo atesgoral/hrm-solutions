e07cc49e7f2541fe7f44b97549f87611cb4839ba
Weilin <934587911@qq.com>
2016-09-07 12:55:16 +0800

    faster 9 steps program in year 21 and 22 steps for year 40

---
+++ solutions/28-Three-Sort-34.78/33.85-steambap.asm
@@ -0,0 +1,45 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 28-Three-Sort - SIZE 33/34 - SPEED 85/78 --
+
+    JUMP     d
+a:
+    COPYFROM 2
+b:
+    OUTBOX  
+    COPYFROM 1
+c:
+    OUTBOX  
+d:
+    INBOX   
+    COPYTO   0
+    COPYTO   1
+    INBOX   
+    SUB      0
+    JUMPN    e
+    ADD      0
+    COPYTO   1
+    JUMP     f
+e:
+    ADD      0
+    COPYTO   0
+f:
+    INBOX   
+    COPYTO   2
+    SUB      0
+    JUMPN    g
+    COPYFROM 0
+    OUTBOX  
+    COPYFROM 2
+    SUB      1
+    JUMPN    a
+    COPYFROM 1
+    OUTBOX  
+    COPYFROM 2
+    JUMP     c
+g:
+    COPYFROM 2
+    OUTBOX  
+    COPYFROM 0
+    JUMP     b
+
+
---
+++ solutions/40-Prime-Factory-28.399/22.423-steambap.asm
@@ -0,0 +1,32 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 40-Prime-Factory - SIZE 22/28 - SPEED 423/399 --
+
+    BUMPUP   24
+a:
+    INBOX   
+b:
+    COPYTO   0
+    COPYFROM 24
+    COPYTO   20
+c:
+    BUMPUP   20
+    COPYFROM 24
+    COPYTO   21
+    COPYFROM 0
+d:
+    SUB      20
+    JUMPN    c
+    JUMPZ    e
+    COPYTO   15
+    BUMPUP   21
+    COPYFROM 15
+    JUMP     d
+e:
+    COPYFROM 20
+    OUTBOX  
+    BUMPDN   21
+    JUMPZ    a
+    BUMPUP   21
+    JUMP     b
+
+
