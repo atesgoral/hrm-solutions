7bfc69e7e5b1f480e0fc315bb0d3a65ad64c7fbd
Ates <ates@magnetiq.com>
2017-09-03 15:44:13 +0000

    Update 2017-09-03T15:44:10.191Z

---
+++ data/32/98f052bab2e9e3c2c519b964ad2a4298.js
@@ -0,0 +1,11 @@
+callback({
+  "levelNumber": 32,
+  "size": 13,
+  "steps": 50,
+  "successRatio": 1,
+  "type": "exploit",
+  "author": "837951602",
+  "hash": "98f052bab2e9e3c2c519b964ad2a4298",
+  "path": "32-Inventory-Report-16.393/13.50.exploit-837951602.asm",
+  "source": "-- HUMAN RESOURCE MACHINE PROGRAM --\n\n    BUMPUP   14\n    BUMPUP   14\n    ADD      14\n    COPYTO   19\n    JUMP     b\na:\n    ADD      19\n    ADD      14\n    OUTBOX  \nb:\n    INBOX   \n    SUB      4\nc:\n    JUMPN    a\n    SUB      19\n    JUMP     c\n\n\nDEFINE LABEL 14\neJwTYWBgiCw46ylYvMPqbImRmVu5nnFt1Xtj47pSi1WN9vbKLd4eCq2SQYtatOKWN+3N3lQXUClYXDU9\nLq9/2a38SyvsqgKW76qevHJrbcyG3IbHWxhGwSgYBUMKAAC1dCLO;\n\nDEFINE LABEL 19\neJwTYGBg2JFy1NkqZUmkfZJgFUNSaod9UumUmmTnedtSeZe+zZi8ckHupRXBRd5Laqt+TAUqZ/hTtTPD\nrPbPjLUNDKcYRsEoGAVDGgAAU3Ybpw;\n"
+});
