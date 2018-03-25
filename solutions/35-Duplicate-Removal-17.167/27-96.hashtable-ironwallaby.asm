0b3bb592ee0c726d26ac012b9ebb0d0afa7654ac
J. T. L <ironwallaby@gmail.com>
2018-03-24 22:24:42 -0400

    Add hashtable-based solution to Duplicate Removal

---
+++ solutions/35-Duplicate-Removal-17.167/27-96.hashtable-ironwallaby.asm
@@ -0,0 +1,35 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+    COPYFROM 14
+    COPYTO   0
+    COPYTO   1
+    COPYTO   2
+    COPYTO   3
+    COPYTO   5
+    COPYTO   6
+    COPYTO   7
+    COPYTO   8
+    BUMPUP   14
+    BUMPUP   14
+    BUMPUP   14
+    BUMPUP   14
+    COPYTO   11
+    INBOX   
+    JUMP     b
+a:
+    COPYFROM 10
+b:
+    COPYTO   [11]
+    OUTBOX  
+c:
+    INBOX   
+    COPYTO   10
+    SUB      [14]
+    ADD      14
+    COPYTO   11
+    COPYFROM [11]
+    JUMPZ    a
+    JUMP     c
+
+
+
