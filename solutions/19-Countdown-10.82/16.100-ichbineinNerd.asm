1e3cd5813dc2ad79adbd0d49d392479a8f2f64af
ichbineinNerd <34607504+ichbineinNerd@users.noreply.github.com>
2018-05-05 16:50:24 +0200

    new Solution to 19-countdown

---
+++ solutions/19-Countdown-10.82/16.100-ichbineinNerd.asm
@@ -0,0 +1,28 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+
+    JUMP     e
+a:
+b:
+c:
+    COPYTO   1
+    OUTBOX  
+    BUMPUP   1
+    JUMPN    c
+    JUMPZ    b
+    JUMP     f
+d:
+    OUTBOX  
+e:
+f:
+g:
+    INBOX   
+    JUMPZ    d
+    JUMPN    a
+h:
+    COPYTO   0
+    OUTBOX  
+    BUMPDN   0
+    JUMPN    g
+    JUMP     h
+
+
