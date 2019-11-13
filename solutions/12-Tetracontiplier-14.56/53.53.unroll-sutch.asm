cf94e1286eb29d71ce575ea66f5266e485d826d8
Dennis Sutch <dennis@sutch.com>
2019-11-13 06:49:16 -0500

    Add non-input specific solution for level 12

---
+++ solutions/12-Tetracontiplier-14.56/53.53.unroll-sutch.asm
@@ -0,0 +1,60 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 12-Tetracontiplier - SIZE 53/14 - SPEED 53/56 --
+
+-- This solution is non-input specific (it unrolls nanashi-juanto's solution)
+
+a:
+    INBOX   
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    ADD      0
+    ADD      0
+    ADD      0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    ADD      0
+    ADD      0
+    ADD      0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    ADD      0
+    ADD      0
+    ADD      0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    COPYTO   0
+    ADD      0
+    ADD      0
+    ADD      0
+    ADD      0
+    OUTBOX  
+    JUMP     a
+
