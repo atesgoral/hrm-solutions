ae183d1c3d3728ac7e358fcf6fd187cfdb318595
Dennis Sutch <dennis@sutch.com>
2019-11-13 06:29:16 -0500

    Add non-input specific solution for level 9

---
+++ solutions/09-Zero-Preservation-Initiative-5.25/11.21.unroll-sutch.asm
@@ -0,0 +1,23 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 09-Zero-Preservation-Initiative - SIZE 10/5 - SPEED 21/25 --
+
+-- This solution is non-input specific
+
+a:
+b:
+    INBOX   
+    JUMPZ    c
+    JUMP     a
+c:
+d:
+e:
+f:
+    OUTBOX  
+    INBOX   
+    JUMPZ    d
+    INBOX   
+    JUMPZ    e
+    INBOX   
+    JUMPZ    f
+    JUMP     b
+
