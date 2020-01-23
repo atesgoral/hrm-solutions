6e04898050b1aa93cc49a24c0349f92474d90fc9
viamodulo <aurelienhmarion@gmail.com>
2020-01-23 22:43:49 +0100

    Create 14.25.viamodulo.asm

---
+++ solutions/13-Equalization-Room-9.27/14.25.viamodulo.asm
@@ -0,0 +1,25 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 13-Equalization-Room - SIZE 14/9 - SPEED 25/27 --
+
+-- Based on 13.25.specific-Gimlao.asm. An extra jump makes it non-specific.
+
+    JUMP     c
+a:
+b:
+    COPYFROM 0
+    OUTBOX  
+c:
+d:
+    INBOX   
+    COPYTO   0
+    INBOX   
+    SUB      0
+    JUMPZ    a
+    INBOX   
+    COPYTO   0
+    INBOX   
+    SUB      0
+    JUMPZ    b
+    JUMP     d
+
+
