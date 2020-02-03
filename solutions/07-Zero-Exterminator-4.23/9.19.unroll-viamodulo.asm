8cfc2b0f2cd9c0239efe24cc96b59d763437e0e2
viamodulo <aurelienhmarion@gmail.com>
2020-02-03 22:42:17 +0100

    Create 9.19.unroll-viamodulo.asm

---
+++ solutions/07-Zero-Exterminator-4.23/9.19.unroll-viamodulo.asm
@@ -0,0 +1,18 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 07-Zero-Exterminator - SIZE 10/4 - SPEED 19/23 --
+
+-- Based on 10.19.unroll-sutch.asm
+
+a:
+b:
+c:
+    INBOX   
+    JUMPZ    a
+d:
+    OUTBOX  
+    INBOX   
+    JUMPZ    b
+    OUTBOX  
+    INBOX   
+    JUMPZ    c
+    JUMP     d
