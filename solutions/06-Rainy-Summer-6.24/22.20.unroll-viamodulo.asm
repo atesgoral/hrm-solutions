403cf616c3b5f2df27f78b6de83ddc38a1881797
viamodulo <aurelienhmarion@gmail.com>
2020-02-03 22:38:18 +0100

    Create 22.20.unroll-viamodulo.asm

---
+++ solutions/06-Rainy-Summer-6.24/22.20.unroll-viamodulo.asm
@@ -0,0 +1,30 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 06-Rainy-Summer - SIZE 22/6 - SPEED 20/24 --
+
+-- Based on 26.20.unroll-mrflip.asm
+
+    INBOX   
+a:
+    COPYTO   0
+    INBOX   
+    ADD      0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    INBOX   
+    ADD      0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    INBOX   
+    ADD      0
+    OUTBOX  
+    INBOX   
+    COPYTO   0
+    INBOX   
+    ADD      0
+    OUTBOX  
+    INBOX   
+    JUMP     a
+
+
