b6a15f9d3b8ea7e8d1ded296fdd5b0e60d97d7e6
viamodulo <aurelienhmarion@gmail.com>
2020-02-03 23:51:10 +0100

    Create 50.68.unroll-viamodulo.asm

---
+++ solutions/25-Cumulative-Countdown-12.82/50.68.unroll-viamodulo.asm
@@ -0,0 +1,74 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 25-Cumulative-Countdown - SIZE 50/12 - SPEED 68/82 --
+
+ -- Unrolled 11.79-skwasjer.asm
+
+    INBOX   
+    JUMPZ    k
+    COPYTO   1
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    a
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    d
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    b
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    c
+    ADD      0
+    JUMP     m
+a:
+b:
+c:
+d:
+e:
+f:
+g:
+h:
+i:
+j:
+    COPYFROM 0
+k:
+l:
+    OUTBOX  
+    INBOX   
+    JUMPZ    l
+    COPYTO   1
+m:
+n:
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    e
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    h
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    f
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    g
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    i
+    ADD      0
+    COPYTO   0
+    BUMPDN   1
+    JUMPZ    j
+    ADD      0
+    JUMP     n
+
+
+
+
+
