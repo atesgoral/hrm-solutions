e7429dc095c12ad8e01677eb744412e96e9c36bf
viamodulo <aurelienhmarion@gmail.com>
2020-02-03 22:48:09 +0100

    Create 13.20.unroll-viamodulo.asm

---
+++ solutions/09-Zero-Preservation-Initiative-5.25/13.20.unroll-viamodulo.asm
@@ -0,0 +1,27 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 09-Zero-Preservation-Initiative - SIZE 13/5 - SPEED 20/25 --
+
+-- Based on 11.21.unroll-sutch.asm
+
+a:
+b:
+    INBOX   
+    JUMPZ    c
+    INBOX   
+    JUMPZ    f
+    JUMP     a
+c:
+d:
+e:
+f:
+g:
+    OUTBOX  
+    INBOX   
+    JUMPZ    d
+    INBOX   
+    JUMPZ    e
+    INBOX   
+    JUMPZ    g
+    JUMP     b
+
+
