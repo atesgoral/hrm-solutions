2693fcd8755ae97fe28e634a7fb9e581f96b1c26
viamodulo <aurelienhmarion@gmail.com>
2020-02-16 10:35:20 +0100

    Created 89.40-viamodulo.asm.

--- solutions/39-Re-Coordinator-14.76/89.40-viamodulo.asm
+++ solutions/39-Re-Coordinator-14.76/89.40-viamodulo.asm
@@ -3,15 +3,16 @@
 
 -- Based on 35.44-Otto42.asm
 
+    BUMPUP   14
     INBOX   
 a:
 b:
 c:
 d:
     SUB      15
-    JUMPN    g
+    JUMPN    q
     SUB      15
-    JUMPN    f
+    JUMPN    k
     SUB      15
     JUMPN    e
     COMMENT  0
@@ -20,35 +21,103 @@ d:
     OUTBOX  
     BUMPUP   15
     INBOX   
-    JUMP     b
+    SUB      15
+    JUMPN    r
+    SUB      15
+    JUMPN    l
+    SUB      15
+    JUMPN    f
+    OUTBOX  
+    BUMPDN   15
+    OUTBOX  
+    BUMPUP   15
+    INBOX   
+    JUMP     c
 e:
+f:
+g:
+h:
+i:
+j:
     COMMENT  1
     ADD      15
     OUTBOX  
     BUMPUP   14
-    BUMPUP   14
     OUTBOX  
     BUMPDN   14
-    BUMPDN   14
     INBOX   
-    JUMP     a
-f:
+    SUB      15
+    JUMPN    s
+    SUB      15
+    JUMPN    m
+    SUB      15
+    JUMPN    g
+    OUTBOX  
+    BUMPDN   15
+    OUTBOX  
+    BUMPUP   15
+    INBOX   
+    SUB      15
+    JUMPN    v
+    SUB      15
+    JUMPN    p
+    SUB      15
+    JUMPN    j
+    OUTBOX  
+    BUMPDN   15
+    OUTBOX  
+    BUMPUP   15
+    INBOX   
+    JUMP     b
+k:
+l:
+m:
+n:
+o:
+p:
     COMMENT  2
-    ADD      15
-    OUTBOX  
-    BUMPUP   14
-    OUTBOX  
-    BUMPDN   14
-    INBOX   
-    JUMP     c
-g:
-    COMMENT  3
     ADD      15
     OUTBOX  
     COPYFROM 14
     OUTBOX  
     INBOX   
+    SUB      15
+    JUMPN    t
+    SUB      15
+    JUMPN    n
+    SUB      15
+    JUMPN    h
+    OUTBOX  
+    BUMPDN   15
+    OUTBOX  
+    BUMPUP   15
+    INBOX   
     JUMP     d
+q:
+r:
+s:
+t:
+u:
+v:
+    COMMENT  3
+    ADD      15
+    OUTBOX  
+    BUMPDN   14
+    OUTBOX  
+    BUMPUP   14
+    INBOX   
+    SUB      15
+    JUMPN    u
+    SUB      15
+    JUMPN    o
+    SUB      15
+    JUMPN    i
+    OUTBOX  
+    BUMPDN   15
+    OUTBOX  
+    BUMPUP   15
+    INBOX   
+    JUMP     a
 
 
 DEFINE COMMENT 0
@@ -68,11 +137,12 @@ eJwTZWBgmFb/xXNZ5YvgdWVfY/xKLqYvKmSq3FeQ2sFVKDMvq7h+rVgZ66agmj1b2lr3bDHquLL5XpfZ
 hl19lxfM6/8xla0/taOsOzzHr/1rjFNTe/i1KsZghlEwCkbBkAEA/98oyg;
 
 DEFINE LABEL 14
-eJwTY2BgOF722Syw+Lvl0+w+L51MyaAP6UsijdI3JL3NEKzKzHrcppKT29dabj3DsqZu1svGvTPnt1ZN
-n95ROJGvZ36Lb49g1cWuNfH3WrvcF7W4uxbUszhbV/+xYRgFo2AUDAkAAJ/ZKII;
+eJzjZGBgCMxd6NNQnOAgVuttOKF+qk5twwWNXY331Jc3TdWJaHK8xjAKRsEoGLYAAPboDjc;
 
 DEFINE LABEL 15
 eJyTYmBgyMuYrVqYnqBpkrbBpCUhxE8oekvqg1Ce8tSQ2Y3rA3L7PvtZz/jsd3DO+oCj85eEHJ1vkvZj
 qnNF4USgVoavqY+0tqU+1y1M5/HckO5evDpTsW1F1s5pWlkhq1Zk9V9ZnSl7/1Vm5nOGUTAKRsGgBAAX
 ji7h;
+
+
 
