665d0d60b1ac9eb024996ca292fa8e7c10e56106
ltdk <usr@ltdk.xyz>
2023-04-04 01:03:42 -0400

    Add new (obsolete) solution for 24-Mod-Module
    This solution was already beaten by another one, so, it's marked as obsolete.

---
+++ solutions/24-Mod-Module-12.57/12.54.obsolete-clarfonthey.asm
@@ -0,0 +1,32 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 24-Mod-Module - SIZE 12/12 - SPEED 54/57 --
+
+-- This solution is superseded by 11.50-jwueller.asm being funcitonally same
+-- with further optimization.
+
+    JUMP     b
+a:
+    ADD      1
+    OUTBOX
+b:
+    INBOX
+    COPYTO   0
+    INBOX
+    COPYTO   1
+    COPYFROM 0
+c:
+    SUB      1
+    JUMPN    a
+    COPYTO   0
+    JUMP     c
+
+
+DEFINE LABEL 0
+eJyTZWBgeJB+zX9eRqpzd+Ze27Tsz2b6eXcMnxbuNvAr6TNeX8ngsLwmwPthXXHkxRaVyQ8nLHiQPmXN
+yy1T417vXlJ3v3pVwNUTq09e+L469az6muOHM1f/2wM0kkGzLODqn6oLm2a1f67eNz1M9+TMJwrPZgnK
+ic5eI8UwCkbBKBg0AAD6QTu9;
+
+DEFINE LABEL 1
+eJyTZGBgsEm2cMvLOJzSnbkzY0HukgLD4iUF4uU92Q2VIald1e4xkjVvQ9hqDcK/1HbFKdeZFF6viZmZ
+W6W1a0eZwcNLRavu3Mo/ecEnX+m0T778vrDCxM08pYdXMrb+mJrUNXlS0bTJkxhGwSgYBYMWAACEly3X
+;
