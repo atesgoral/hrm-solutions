2b0dfc3641c47606e3d2956c5f139c29be2cd7eb
ltdk <usr@ltdk.xyz>
2023-04-04 01:23:23 -0400

    Add new solution for 38-Digit-Exploder
    I *hated* the speed challenge for this one since the solution wasn't super obvious and felt input-specific. This is the best I could attempt for a "natural" optimisation.

---
+++ solutions/38-Digit-Exploder-30.165/82.162-clarfonthey.asm
@@ -0,0 +1,186 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 38-Digit-Exploder - SIZE 82/30 - SPEED 162/165 --
+
+    COPYFROM 10
+    ADD      10
+    COPYTO   7
+    COPYFROM 11
+    ADD      11
+    COPYTO   8
+a:
+b:
+c:
+    COPYFROM 9
+    COPYTO   3
+    COPYTO   4
+    INBOX
+    SUB      11
+    JUMPN    m
+    COMMENT  0
+    COPYTO   5
+    BUMPUP   3
+    COPYFROM 5
+d:
+    SUB      8
+    JUMPN    e
+    COPYTO   5
+    BUMPUP   3
+    BUMPUP   3
+    COPYFROM 5
+    JUMP     d
+e:
+    ADD      11
+    JUMPN    f
+    COPYTO   5
+    BUMPUP   3
+    JUMP     g
+f:
+    ADD      11
+g:
+    COPYFROM 3
+    OUTBOX
+    COPYFROM 5
+    SUB      10
+    JUMPN    k
+    COPYTO   5
+    BUMPUP   4
+    COPYFROM 5
+h:
+    SUB      7
+    JUMPN    i
+    COPYTO   5
+    BUMPUP   4
+    BUMPUP   4
+    COPYFROM 5
+    JUMP     h
+i:
+    ADD      10
+    JUMPN    j
+    COPYTO   5
+    BUMPUP   4
+    JUMP     l
+j:
+    ADD      10
+k:
+    COPYFROM 4
+l:
+    OUTBOX
+    COPYFROM 5
+    OUTBOX
+    JUMP     c
+m:
+    ADD      11
+    COMMENT  1
+    SUB      10
+    JUMPN    r
+    COMMENT  3
+    COPYTO   5
+    BUMPUP   4
+    COPYFROM 5
+n:
+    SUB      7
+    JUMPN    o
+    COPYTO   5
+    BUMPUP   4
+    BUMPUP   4
+    COPYFROM 5
+    JUMP     n
+o:
+    ADD      10
+    JUMPN    p
+    COPYTO   5
+    BUMPUP   4
+    JUMP     q
+p:
+    ADD      10
+    COPYTO   5
+q:
+    COPYFROM 4
+    OUTBOX
+    COPYFROM 5
+    OUTBOX
+    JUMP     b
+r:
+    COMMENT  2
+    ADD      10
+    OUTBOX
+    JUMP     a
+
+
+DEFINE COMMENT 0
+eJzLYGBg2GQwwzrbXN1X0n5P6lPnB+XfvBvmcfhvWibjO3NRkjfzolUuRxfPMfdd16f1fg9QOYODxufb
+KwzfXhFzqT13KODF2ZCw8ydB4v4JOjm+qZZJhzKkwmfmKLpJ5GuYMhc+T/tZmLswsLh+G0iNddKE06GZ
+L87+LHxxNrCb90RXv8LR0olFB0ByM+YcsUmfs9rCYXaDufTUiT7Zkwoj3/S7FHT176p90/9kmtWkx+u+
+TK7f5jStde/JOT7Hohb9PtW+Zs+Z5xt/n3q8O/6wypFJm9mOJ6z+eCJ3oecp/2mep2T7tp283Mh2vDnP
+90BL2NSdfJ6bFj/RB9l1vPOAbHTXDcXkvh16r6ZUeHyYe6vgx8KwVv3V7/cUr/t96sCGaZfsN6+/FrTN
+6BbPDqNbID3zX7zwq3ju6/7+6TeXO48SXD4+UHSbeLvO3/lmS5ja9cWJOVdVarmuGnTnXE2fteu66HaV
+O0Fn+e9Nu6TzIOLK+6fC55Nf8p6Q/JqwOv27/mLxX0mTxX/ld4LMrThfZXDsoqOlzgPBhsPvIq54fb35
+/uT3hT+e/Pr/n2EUjAIaAwCSXdJU;
+
+DEFINE COMMENT 1
+eJzzZWBgSHNss2u3jAyVN7iSGat1tqpNLb/zk+qyGW1qk2f/0myYV2+qv/iI94WNQKUMNxKfp81Ozw7a
+kenrfiU7xCwj57F7Rk5j28dczpUg+alxcw82VLzfA2K/6f/mYtNr4izf7etu0hnst7UtLsasJbaEtcWz
+X6e1bemzNocd+R1S+zZ3fz3M3dt1ZHd/1xHVKUUHWme+3PphrvtyuwUlE1bNr6nnn/0r+dWUF35v+u86
+cPcesQGZ3dbo684/W30LiM1xKDtI5oC6r+ueiT5MO68F7tvqHW+65UrmgQ1cE+M2+K57vlFq3/Qte85M
+3Xn7+oo9J25NPBx7e/Uphks8FzbsO3bxwsZjFyfPXnn+TeuBE4FpAscU4iwPuUWAzA1bcy3w9HrhDK69
+29dyHFI7I3W0+eqBEyduMYyCUTCMAABQE5I/;
+
+DEFINE COMMENT 2
+eJwzYmBgWBhxLTAhQC01wKO/Zp9T0uQ0x8mzCx3cl7M4mq7SdO9Zs8H3wka+iNa9QKUMfbnP05LLHrt/
+L0vz1ij3KmcsK14BEq/KZ9olV1F0gLnz62EQ//nGbXGn18fF6K+Oi6lfoZZ6fOH3ycYLi1e8W7R0g/ry
+l1s/bRDd7rF90ua3ux6vO7KvYZ7vAdm+hweOF8scOJjzdd+VzH1bjaNB5kxcPTfKZ21z3qcNH3tW7Hl7
+BSTmt6LB/OEKPs/rK9V9Vdf8Sr4PlGcYBaNgFJAFAMX9XZQ;
+
+DEFINE COMMENT 3
+eJyzYmBgUDdxtHxl6ev+wUktdZVLbMkql8uNYi5hrScd8zt3WHJNdDOcPPuZ9tHF2lrFK4DKGULULE8W
+akw4PcV4wuk428UnWBy3HQWJH8rwjhfIdotYWODuwF2qdgYiVnvuQe3iEzqtJw+B+BcX3HWYMUfPpnqW
+nk3rzA+27NMV3R5MVfd9NSUl+dWU5Or4GcyLzOYw7RKfP/eg25K5B0tXbti3ZF39tgMblm6w3xy15PJm
+5akZ6z+X7Fo+0YdpoahF+pxe3e0zF2jNmn5aG2T+pMlS4Z9muBTMWxoy9/Dq+m3bNi4+Ubl9/TWGUTAK
+RgFWAACrAWaL;
+
+DEFINE LABEL 3
+eJyTY2BgeJh6LfBI8m/vnKQLXu4xad7bwnRD4kJPRh8KeNNq6R8y19K/eIV+QPBWgeA7+/tD4w/vizp5
+yDjuzv7/STN21WT1rDmQG7VkX5HZpPmlNfVGZVvKGMsWJ4qVqPteAPKPlcX0NFRUTfetLV4xocFqo2TT
+pM1V7X82MYyCUTAKBgUAAPGwObw;
+
+DEFINE LABEL 4
+eJwTZmBgmJkzz6kvt8KjJe9kdEverlqdXN4TH3PTNhUUhMx9X8Q1cX6pQfeZ+sa2e035nWYtSZNntMxc
+dK9p6YYHtVL7squ6jmSle9+rS31wj2EUjIJRMOQAAPxsJds;
+
+DEFINE LABEL 5
+eJwTZGBg2N/K5zmhYZ5TZPkpZ96CJZ4fcyf6rM06HPI5gz2KPf1XcmeKV3lnyrFuobT0WWfS9RcLZCes
+biq+sDG/Q32L0zT+DQyjYBSMgiELAOsgHaM;
+
+DEFINE LABEL 7
+eJyzYGBg0HS/krnHXS31m/e7UA7/7KCi4GuBYjFzo1LivOPnJrkU+KYeaHmYWjKhLnXTsrlJJw+JJJ4/
+GRj9+1RMVMXxE5EcB3ujmHa5x0zaPDVu6Ya38Us3JCXXb9uR+X7Pz8L3e4DGMyybeCVzSp9dpnw3Q/aC
+Dsn84LYtZSdbbnbotB5d7NE+9+DxzmmXbvV43zvfu+hB6UTve+8WnTyUsPrCxnVrE1brbshdqLshfZbT
+Otm+s6umNMuuCKgTn3+roLzHOBpkdmpnnX9T95eYw5P/NwlOWzAxfc7SDdMW+hzbtbzghstKo1sMo2AU
+jAK8AACPhHCr;
+
+DEFINE LABEL 8
+eJzzZGBgmGd0LdDQeK+vi9lj9zxXX3c/3yWe3kHXAhcHx8UsDo4t+RzQ3lvvy7xI3iNtU7Oz7u7TNtuO
+vrLsOnLW/M+mOeamq/QtZJbNspm5KMYpd+Frnw9L4kLrtylER+4RSdywD2g8w/xS4+h9RV2xlwrOJRUU
+sBWlFkYt0SryXRdYvHdLclnr3t3lkXu+Vvesyay3mZlZX1O/tO5zycYKt4jksunB3KWMwSAztuZdCzQp
+nJZ9rMygW60mZK5He85OkLjlKoU4w2VuEaJLCiONF26L2zJvWnbfXLkyljmb+xbN3j/n5Jxvqy8ueLl1
+3tL3e/xWvN9zdV3apucb56803ZK7sHmb8tSn2wy612zaUjZnVVds/Qr2qHeLCiNB5lbPmuizafHxYoZ9
+hy4zjIJRMIwBAJlLiGw;
+
+DEFINE LABEL 9
+eJyTZ2BgsKpabVFRomejl2Hl9Tb+XahxHHuUe4xaamD08eLAaNtGxriq6a6JxSvWpzrsuJd18hBz4baj
+eaXbjmbWb9jH2uKw407743WXOkLmfmg9XqzT+jZ3Vn1kaF3tC7/E6t/eQOMZ9kXd02hImOeUlf48Lbro
++2SjsjXzmTsVjjKMglEwCgYcAAAeZDgk;
+
+DEFINE LABEL 10
+eJzTYWBg4PA/W7UtrCs2JuqCl0L0KeeYqO+Te6O+rW6OvrO/OXrOTaAShhwv1+vxGQU3VudLXgPx3Xq6
+XcXb+TwXN18L/NTgHS9T+6xOrSa/82t1wzynOquNExpWbe9rNT4k3516irtX7YzLRLUzB2fMPeg1q3Xv
+yTlWGz/MzV34Ye6x7l9zzKskZ/EmWkyfHpw4eYnnownfXHq67jqA7HhVk+BiUb8qwKF5Wxz/7K+H3Zao
+PmGgA+Bd8Gpu7doH9z5tmPrq8mbn9/SwcxSMgoECAAslXGo;
+
+DEFINE LABEL 11
+eJzzY2BgyPF6njYx4F1obdBe31MhHE5KYdZGnuEb7SXCkybfDT+6GKiEodqeaRdQfktZ4qlV8RkJq0Fi
+E2bkKbFPv6G4ccIOveOdCS46rW4RnxoMiy3qZfss6tNntTUu3XClecauH52pp771Tbu0bOLbK5nTI654
+zWK4dHt+xXGupQ479Fc3zJNe69n/cs2cItkVuiF7F9112DIvxKx1prKe9NQFWiB7BOsSdfQaVlt4N220
+j+yvqXea9mruxQU+x7iW6lzesCL2NkhNykVfd7Hzp5xjzp5y/n18os/Ew9visg7ZZV4/aFjMcWhXLdvx
+njXPT77fk3Lx96ld11ee/3z7xVmz+z7HFJ4preV+wbyI+4Xy1FvPPfu3PpyQwnq/MPL/VUU344t3HUBm
+R+w/YlN0rM7/ziPR7SeeFB1wf+5zjGEUjIJhAgDz8pYQ;
