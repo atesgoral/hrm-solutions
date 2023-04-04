6bc3a98b32c17673bf28afa8727ce50765458e0f
ltdk <usr@ltdk.xyz>
2023-04-04 00:50:48 -0400

    Add new (obsolete) solution for 20-Multiplication-Workshop
    This solution is already beaten by an existing one, so, I marked it as obsolete.

---
+++ solutions/20-Multiplication-Workshop-15.109/15.160.obsolete-clarfonthey.asm
@@ -0,0 +1,48 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 20-Multiplication-Workshop - SIZE 15/15 - SPEED 160/109 --
+
+-- This solution is superseded by 15.135-skwasjer.asm being functionally same
+-- with further optimization.
+
+    JUMP     b
+a:
+    COPYFROM 2
+    OUTBOX
+b:
+    COPYFROM 9
+    COPYTO   2
+    INBOX
+    COPYTO   0
+    INBOX
+    COPYTO   1
+c:
+    JUMPZ    a
+    COPYFROM 2
+    ADD      0
+    COPYTO   2
+    BUMPDN   1
+    JUMP     c
+
+
+DEFINE LABEL 0
+eJwTZmBguJqancWf8T5RKisz1rKgOHJrYXGkfMmNmMiykNSl5RdyBetn7vzWZHnBoLXmkkZH27k5PfL7
+gNoYdDI7bqvkHD9sViuYfLMx355hFIyCUTDkAABygB/z;
+
+DEFINE LABEL 1
+eJyTZmBg+Jqda1SYG+UuUnQ4ZXapbI5ipX1ubMPhFLXmX7HGXTUeR/q1DJomtJgLTBRMTu3PXLSz1/yw
+QveWG886ih+vaZv65GZjzD3JmqiLu6qdz6TWLjtUUP9vz/Smo9s8+gRXCE/bviBl4fzZnkutZzCMglEw
+CgYdAAA6ETPN;
+
+DEFINE LABEL 2
+eJzTY2BgEAjLjD0XxhL1KW2vLX+Gju3uTAaH5hwmX/284shTpZmLJlfYb7peI3roYV3VUaByhrb42cfK
+UtUWbC18n/irNMo9oDJMFyTO1r/CNWjCFr9jUya2XpzG2/du+oMJVjMLJxrNqem9O3dTm+fSNfGFK5yd
+ZFYlaHKtPqrjsvaK068Ny2o7NgquyN4gv89v/exjW9esOD97nu2r8FnTP/ZPn/5ReFrDG4bpjx/Jzqi7
+Lz93y42oE2Hbf1xp2sIwCkbBKKAKAADTxFgP;
+
+DEFINE LABEL 9
+eJxTYGBg6G89qqPWbKKTWjtTn6Xikmlx0USH6vwAb/Zso5QH6cfrPqWd7P6UNn/2vIzDK79mP9/+P/ff
+ngPFEUeEW3SOX+uLOHJz8ucDoVPl9zFMf75ddsb0NbIz3Bc6TSucOH9if8Ws9lVBsQ1cNkBrGOLyXlqZ
+VCTkPesQ3srWH3BVYGLxYwY6A7utK7oqtlvPeLM7ZNWTfbIbPx712d10+vl26XNNW0Iv3Fp38aLgCt/L
+B+dwXznZveby3qaii0cLq84JJr88YR28/tg1f8YDhT6r9lzzr9j+KAJknsiLnWtB9PM39rl2H8ws7T48
+1214r2Xw/M1JN79X9rlhz83qvj9jaAWp2ftk59oZz6avmfDyz/pLL3x2i7zo3/v9We4ey2ccOy2fNW0J
+e26/Kew5x056h8koGNkAACDLlqA;
