bf7bf8b420b39e80d9386a1937502acf0524037e
atesgoral <ates@magnetiq.com>
2023-08-20 19:38:20 +0000

    Deploying to gh-pages from @ atesgoral/hrm-solutions@c24b71a92d53fd4f36fa942d6f7b93b1e5f5749a 🚀

---
+++ data/23/fcaa6ce65372d7b30722427f3f8d26d5.js
@@ -0,0 +1,12 @@
+callback({
+  "levelNumber": 23,
+  "size": 43,
+  "steps": 63,
+  "successRatio": 1,
+  "legal": true,
+  "worky": true,
+  "author": "icez",
+  "hash": "fcaa6ce65372d7b30722427f3f8d26d5",
+  "path": "23-The-Littlest-Number-13.75/43.63-icez.asm",
+  "source": "-- HUMAN RESOURCE MACHINE PROGRAM --\n-- 23-The-Littlest-Number - SIZE 43/13 - SPEED 63/75 --\n\n    INBOX\n    COPYTO   0\na:\nb:\nc:\n    INBOX\n    JUMPZ    l\n    SUB      0\n    JUMPN    d\n    INBOX\n    JUMPZ    n\n    SUB      0\n    JUMPN    f\n    JUMP     b\nd:\ne:\nf:\ng:\nh:\ni:\nj:\nk:\n    ADD      0\n    COPYTO   0\n    INBOX\n    JUMPZ    p\n    SUB      0\n    JUMPN    h\n    INBOX\n    JUMPZ    q\n    SUB      0\n    JUMPN    i\n    INBOX\n    JUMPZ    r\n    SUB      0\n    JUMPN    j\n    INBOX\n    JUMPZ    s\n    SUB      0\n    JUMPN    k\n    JUMP     a\nl:\nm:\nn:\no:\np:\nq:\nr:\ns:\n    COPYFROM 0\n    OUTBOX\n    INBOX\n    COPYTO   0\n    INBOX\n    JUMPZ    m\n    SUB      0\n    JUMPN    e\n    INBOX\n    JUMPZ    o\n    SUB      0\n    JUMPN    g\n    JUMP     c\n"
+});
