a47a465fde812c82792f7935028f06baa0ebe519
AMob <84364963+Ammmob@users.noreply.github.com>
2023-12-29 16:46:36 +0800

    Create 38.93.specific-AMob.asm
    add a new specific solution

---
+++ solutions/20-Multiplication-Workshop-15.109/38.93.specific-AMob.asm
@@ -0,0 +1,89 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 20-Multiplication-Workshop - SIZE 38/15 - SPEED 93/109 --
+-- This solution is level-specific (quality-limited input) and assumes input
+-- values of 0-9.
+
+    COMMENT  0
+    JUMP     l
+a:
+    ADD      0
+b:
+    ADD      0
+c:
+    ADD      0
+d:
+    ADD      0
+e:
+    ADD      0
+f:
+    ADD      0
+g:
+    ADD      0
+h:
+    ADD      0
+i:
+    ADD      0
+j:
+k:
+    OUTBOX  
+l:
+    COMMENT  1
+    INBOX   
+    JUMPZ    m
+    COPYTO   0
+    INBOX   
+    JUMPZ    k
+    COPYTO   1
+m:
+    BUMPDN   1
+    JUMPZ    i
+    BUMPDN   1
+    JUMPZ    h
+    BUMPDN   1
+    JUMPZ    g
+    BUMPDN   1
+    JUMPZ    f
+    BUMPDN   1
+    JUMPZ    e
+    BUMPDN   1
+    JUMPZ    d
+    BUMPDN   1
+    JUMPZ    c
+    BUMPDN   1
+    JUMPZ    b
+    BUMPDN   1
+    JUMPZ    a
+    INBOX   
+    COPYFROM 9
+    JUMP     j
+
+
+DEFINE COMMENT 0
+eJzbzcDA8EP8nnq4imgakMnApPBWzVwrROGVVZ9Mqc1ns6M2dzw6rOMSZ1m9zJhqdjprg45RiotqXCJI
+7V719IAT+hu89Yw3eGeb7PfiNH3tDhLX1M/Uv6VnHWyjVxMP4n8yFE27pVdc0Kb1r/yCml9zjIZ9ywvN
+mKYT+tmN/JYxTd9NGyYI6egta9M6vsZPV355pb7eMm0HvWUPnaMWn3epWJjrenDOJOeQySCzljq5Tol2
+m70+13X/3knOkw+AxFaYqm2favZ6F7/lv1f7LQ6/MzG3fA4Sl1d/vWu1xsdTqzVyL4P48+2Ong11qrnk
+7Ox4zdmZ7RZILNXPvuVtzINKplzLEhA/MPdfOVPu/r1Hs9fdv5E1/eO6jKZHIPFrkWou5pG/3PaFhRWd
+Ccz+AhJrLb9UJVj5dBWIvauRp7yp5e3Mqc3eS/Qa1LY71HPs7Kru3TC5SH45SN59yqWqv9MvVQXN+Vcu
+NV+rdPLCpnyQeMySiMC+uUYpvjN4yi9Pfd7ZMtV1yrrpb2dmzm9bMGfGov1F066cEZ29/dyFRUfPxiyR
+PfFiaf4mkL5fS1yn8C45OEd+sdp2+cUvb+ctbnrEu8T9Ge+SiQ/BYbIxwcFtU1xi5Mbv6ffXrcsBiV3a
+vC1aZGdNvPLRrjiImgeVltsfVILYIWv1ljmuO/wOxLbc/rWDbZ/3kof7TbY1HNh+ruHAsg/O+6d/PLVn
+63u1HbyvQGr2b1m0f9a2XztBbJkNAVenbnK8BmIrH13Tu/5Y5qJ/x3o3gPg1Z6saOM4XTgw/f2f3znMz
+L+w8p/RT96zfN5DcygePIiIfGKXU3dMqPXdvep37w6qGlBcPKls/v68Ah+/no1GCX09nvflWOBHEr369
+36v69fd09rdfO768fb0L7O47q9aF3Hl8uu5ezaXCx6I3dV4bXU/4evTsol8fT535o3McpObMn88ra/88
+fMMwCrACALgyTks;
+
+DEFINE COMMENT 1
+eJxbxcDA8FCkzk5WLMjxsmSQY6bybBugEMMN2QbZG7JMZi2SuQlFUgFJnbJxiaeVVsSA5IT5TnZrC67p
+varl1yykE9O0QWd6HUi8RqnUn0lhyTwmBZ3jIP5UM0mVJ2YHNWVMVllr6rf4zdB8FAESV7O54+Hhftaz
+yPO1O4h/S29dzlSzdTnz7b6nQ/jPO0/oT51/Qn/VOhD/kvmlKn7L9xWvrC5VMdp97ei1/zOj195563y7
+uCvJtmy3ZlnFXYmzXHH+idmi/SD1s6wOzhGxXTIPxO7W+7xyl8mqdfstZq8H8ae7S5dNdxdub/XYNNfM
+g2klSCzVb11OkH9MU5D/xIdifnvvgcR08hXb7PIaJlxOu7N7dzL/Sa040Zsg8ctpsqHrMnIT7uZJlz0q
+ym4UrLww+3iN+A6QnEQfh5tE336v7X2TfUH8lNm2RgvnrLJmmpPgkDNpv1fU5Ba/ddOtg4umbYs2mPgy
+Q63zfUX89AeVTHP+ldfM0yrNnB9WtGP+rUyQXqcJc6VB6t2nuE4xndq24NkUz6Mg8dVLxF1jlsQ0gdgh
+a3dJOq6LMri/TjQtcuOmuZEbrY9xbXx8+uz6K2eEVpQdAql5c7zHVvnozNjWI3uzPQ7/mTH9cOLmg0fv
+7NY4OfnAvkuTD/BePrfn9+XXu0Bq752ZGctx/nQWAzgBfAxvv+qYLH1ufu6c45YlN0/wlF+7qlUKktp3
+aYP3vktdcbGXVVJjL1csnHll8gGQ+MLTVQ1bTt3o+nfs3iwQ//fl3T3ptysWyt63XQri73iTHnDqtWxo
+4ePchMuPHJMtn02vm/3Udmnh48enQfKdz/yaq19XNfz6+KDy+afD1Vc/xzS9+fZ0lfX3//8ZRgHNAABL
+UiFx;
+
