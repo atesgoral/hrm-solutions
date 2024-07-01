5c3cb0da97b1ead916cabcd1f77885f404d2e9cf
clarfonthey <15850505+clarfonthey@users.noreply.github.com>
2024-07-01 06:02:33 +0000

    Deploying to gh-pages from @ atesgoral/hrm-solutions@509ff84bdfa882196e53a9b4c1bd69c9598be148 🚀

---
+++ data/36/3aa0b2e02f4205c552fcb20fe56194de.js
@@ -0,0 +1,12 @@
+callback({
+  "levelNumber": 36,
+  "size": 25,
+  "steps": 88,
+  "successRatio": 1,
+  "legal": true,
+  "worky": true,
+  "author": "halchihal",
+  "hash": "3aa0b2e02f4205c552fcb20fe56194de",
+  "path": "36-Alphabetizer-39.109/25.88-halchihal.asm",
+  "source": "-- HUMAN RESOURCE MACHINE PROGRAM --\n-- 36-Alphabetizer - SIZE 25/36 - SPEED 88/109 --\n\na:\nINBOX\nCOPYTO [23]\nCOPYTO 19\nJUMPZ f\nBUMPUP 23\nJUMP a\nb:\nSUB 19\nCOPYTO 24\nc:\nd:\ne:\nCOPYFROM [19]\nJUMPZ h\nOUTBOX\nBUMPUP 19\nBUMPUP 24\nJUMPN c\nf:\ng:\nINBOX\nJUMPZ g\nCOPYTO [24]\nCOPYFROM [19]\nJUMPZ i\nSUB [24]\nJUMPZ d\nJUMPN b\nCOPYFROM 24\nCOPYTO 19\nJUMP e\nh:\ni:\n\n\n-- According to previous writer, Azijn-san, 24 SIZE solution exists. --\n-- He explored http://steamcommunity.com/app/375820/discussions/0/405694031552336695/ --\n-- this time writer halchihal --\n\nDEFINE LABEL 10\neJwzYGBgOGR7zy7Z9lJVh/X7ZWst//8HCjGUaT2KWKEfE3LI9pr/Fb9r/tpB3wOUQ2NCQHKT04xSxFMD\nrl5N3bBvQ/qaJTL5B8O/F9yzEynaa+tX0ubSUDk9fW7V86Jd1c+LBOu74ko7F2tP6tIyONPjHnOmh7dv\nffeaJSBzIud/D4ic3/3CZp77Qpt5HCWz5z2KmLxQ0fHVEg59xxU7lMPWvJfxWy8o93yjlsHSTZN9l25K\nyMvesKLr+2q1BRsWcez0nTH7GMMoGAWjgCIAAArGV60;\n\nDEFINE LABEL 23\neJxzZWBg6HUNUdBzVVvQ63ruFZDLkOE+XWm7r5H8vDAj+RsRt1S+RjZp6cfstX0bo+fVFyOYPC3yTxZb\niFZpqV9/BUg9Z1OEVUVNpfWhCgaHsMLvAXvzziao5Ci2Hc2+NuVoNsv8uLyQVbfyb63TLGva8qX2+fb9\nrc+3R3Q0bTnTY7TSYWLAhNhJEfWxk6anz5+4Jl55gnUwX89r94iONhfOJmcnkPnavQeFtHsF5fh6EjSD\n66oaNMuqjmaXvr4IklPbd9LNbutr97Xrt/jtXakWfXSZUUrasj9ZR5dxlOxdealKZK1ZXcX2e82ce/X6\nn+z7MXXR/oNzFu0PWZWx9+g2xS1Kpyes07uyd+WWG4GLJO9Ezr92M3HOyQsMo2AUjCAAAPLWfV4;\n\nDEFINE LABEL 24\neJyzZWBg0BIXtdASn75mneil/UAugx1Lbnm3Xm45iN3qsVj7uHuE1QxTtQWPDM0Pd+sJvt2g0/Rzg87z\nP5+0tf6D1CR4uS+cFum+0CeWZT6If6R/hev67hWu1u2PIhJajVLUms3q1Jofz+Vs+rM+oylh88ve/r0P\nJ4Awx06Q+g2LnJ02LNLrz1s8f3be4v69GxYxnIKIVzU8Wl7VcGjzhsqCXRsqQWKce+/Zce7lvQxir7m8\nwpX7yuuLvpf1roD4Sddzy/8/7oqb8exgeMeba/4gsXs3vMtsb1Y17H2yYd/zNyxP/nyAuHkUjIJRAAEA\nOkhp6Q;\n\n"
+});
