53c9429621e7c959e62d638cfbfd33cdcca62be7
halchihal <halchihal@icloud.com>
2024-07-03 23:44:40 +0900

    Create 18.141-halchihal.asm
    How about something like this?  (twice again)

---
+++ solutions/34-Vowel-Incinerator-13.323/18.141-halchihal.asm
@@ -0,0 +1,26 @@
+-- HUMAN RESOURCE MACHINE PROGRAM --
+-- 34-Vowel-Incinerator - SIZE 18/13 - SPEED 141/323 --
+
+    BHMPUP    5
+    BHMPUP    5
+    ADD       5
+    COPYTO    7
+a:
+b:
+c:
+d:
+    INBOX
+    COPYTO    9
+    ADD       4
+    JUMPZ     d
+    ADD       7
+    ADD       5
+    JUMPZ     c
+    ADD       5
+e:
+    ADD       7
+    JUMPN     e
+    JUMPZ     b
+    COPYFROM  9
+    OUTBOX
+    JUMP      a
