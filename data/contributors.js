b8a19d1dbc2651081883c4cb4c8cb2a85da5003b
atesgoral <ates@magnetiq.com>
2024-07-05 03:03:35 +0000

    Deploying to gh-pages from @ atesgoral/hrm-solutions@6c876805beed400e212835c550baee5d24b7dd77 🚀

---
+++ data/contributors.js
@@ -0,0 +1,316 @@
+callback([
+  {
+    "username": "atesgoral",
+    "fullName": "Ateş Göral"
+  },
+  {
+    "username": "unframework",
+    "fullName": "Nick Matantsev"
+  },
+  {
+    "username": "spenserhale",
+    "fullName": "Spenser Hale"
+  },
+  {
+    "username": "grimer30",
+    "fullName": "Michael Stout"
+  },
+  {
+    "username": "Ferwex"
+  },
+  {
+    "username": "hermanprawiro",
+    "fullName": "Herman Prawiro"
+  },
+  {
+    "username": "IAmWave"
+  },
+  {
+    "username": "valkum",
+    "fullName": "Rudi Floren"
+  },
+  {
+    "username": "codejnki",
+    "fullName": "Patrick Stockton"
+  },
+  {
+    "username": "Patrick-Jakubowski"
+  },
+  {
+    "username": "lzelus"
+  },
+  {
+    "username": "CozyRocket"
+  },
+  {
+    "username": "peterfreese",
+    "fullName": "Peter Freese"
+  },
+  {
+    "username": "polarathene",
+    "fullName": "Brennan Kinney"
+  },
+  {
+    "username": "cowboy",
+    "fullName": "Ben Alman"
+  },
+  {
+    "username": "ekx",
+    "fullName": "Benjamin Dengler"
+  },
+  {
+    "username": "pyaehtetaung",
+    "fullName": "Sai Pyae Htet Aung"
+  },
+  {
+    "username": "Otto42",
+    "fullName": "Samuel Wood"
+  },
+  {
+    "username": "IllegallyBlind"
+  },
+  {
+    "username": "WolfWings"
+  },
+  {
+    "username": "FireGoblin",
+    "fullName": "Michael Overstreet"
+  },
+  {
+    "username": "AlanDeSmet",
+    "fullName": "Alan De Smet"
+  },
+  {
+    "username": "Gimlao"
+  },
+  {
+    "username": "szubsteru"
+  },
+  {
+    "username": "chris18191"
+  },
+  {
+    "username": "Multirez"
+  },
+  {
+    "username": "albertferras"
+  },
+  {
+    "username": "Halling69"
+  },
+  {
+    "username": "AaronKnowles"
+  },
+  {
+    "username": "og01"
+  },
+  {
+    "username": "fd1e4020"
+  },
+  {
+    "username": "Resnox"
+  },
+  {
+    "username": "andrewboudreau",
+    "fullName": "Andrew"
+  },
+  {
+    "username": "mschordan"
+  },
+  {
+    "username": "tuxuin"
+  },
+  {
+    "username": "skwasjer"
+  },
+  {
+    "username": "jlmitch5"
+  },
+  {
+    "username": "ocoss"
+  },
+  {
+    "username": "LRFLEW"
+  },
+  {
+    "username": "johanatan",
+    "fullName": "Jonathan Leonard"
+  },
+  {
+    "username": "Mygod"
+  },
+  {
+    "username": "meh2481",
+    "fullName": "Mark Hutcheson"
+  },
+  {
+    "username": "qefn"
+  },
+  {
+    "username": "psanetra"
+  },
+  {
+    "username": "jwueller",
+    "fullName": "Johannes Wüller"
+  },
+  {
+    "username": "viamodulo"
+  },
+  {
+    "username": "nanashi-juanto"
+  },
+  {
+    "username": "18111398"
+  },
+  {
+    "username": "hastebrot",
+    "fullName": "Benjamin Gudehus"
+  },
+  {
+    "username": "lezardi"
+  },
+  {
+    "username": "henke37"
+  },
+  {
+    "username": "caitsith2"
+  },
+  {
+    "username": "viiri",
+    "fullName": "x0r & jthermit"
+  },
+  {
+    "username": "Eirik0"
+  },
+  {
+    "username": "esseger"
+  },
+  {
+    "username": "michiexile",
+    "fullName": "Mikael Vejdemo-Johansson"
+  },
+  {
+    "username": "gillius",
+    "fullName": "Jason Winnebeck"
+  },
+  {
+    "username": "nhowell",
+    "fullName": "Nick Howell"
+  },
+  {
+    "username": "sniperrifle2004"
+  },
+  {
+    "username": "truhlikfredy",
+    "fullName": "Anton Krug"
+  },
+  {
+    "username": "nelson-antunes",
+    "fullName": "Nelson Antonio Antunes Junior"
+  },
+  {
+    "username": "fbastien",
+    "fullName": "Florian Bastien"
+  },
+  {
+    "username": "jdashton",
+    "fullName": "Daniel Ashton"
+  },
+  {
+    "username": "steambap",
+    "fullName": "Weilin Shi"
+  },
+  {
+    "username": "riophae",
+    "fullName": "Fangzhou Li"
+  },
+  {
+    "username": "eiTTio"
+  },
+  {
+    "username": "tobeannouncd"
+  },
+  {
+    "username": "kyanet"
+  },
+  {
+    "username": "837951602"
+  },
+  {
+    "username": "Azijn"
+  },
+  {
+    "username": "mrflip",
+    "fullName": "Philip Flip Kromer"
+  },
+  {
+    "username": "mathiaswk",
+    "fullName": "Mathias Weirsøe Klitgaard"
+  },
+  {
+    "username": "gjareno"
+  },
+  {
+    "username": "popq",
+    "fullName": "Pop Qvarnström"
+  },
+  {
+    "username": "ironwallaby"
+  },
+  {
+    "username": "ichbineinNerd"
+  },
+  {
+    "username": "landfillbaby",
+    "fullName": "Lucy Phipps"
+  },
+  {
+    "username": "dubaaron",
+    "fullName": "Aaron Wallentine"
+  },
+  {
+    "username": "Whathecode",
+    "fullName": "Steven Jeuris"
+  },
+  {
+    "username": "sutch",
+    "fullName": "Dennis Sutch"
+  },
+  {
+    "username": "azimux",
+    "fullName": "Miles Georgi"
+  },
+  {
+    "username": "FalconX4"
+  },
+  {
+    "username": "marcinsmu",
+    "fullName": "Marcin Smulewicz"
+  },
+  {
+    "username": "ad-pro"
+  },
+  {
+    "username": "clarfonthey",
+    "fullName": "Clar Fon"
+  },
+  {
+    "username": "icez"
+  },
+  {
+    "username": "Ammmob",
+    "fullName": "AMob"
+  },
+  {
+    "username": "szubster",
+    "fullName": "Tomasz Szuba"
+  },
+  {
+    "username": "truhlikfredy",
+    "fullName": "Anton Krug"
+  },
+  {
+    "username": "halchihal"
+  }
+]
+);
