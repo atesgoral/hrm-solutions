137aaedd48b5a8f8d211955cbf3b87950dd9d1a9
Stefan Lobbenmeier <stefan.lobbenmeier@gmail.com>
2025-03-02 15:49:09 +0100

    Add comment about the solution, rename file to match my name

--- solutions/35-Duplicate-Removal-17.167/27.95.specific-StefanLobbenmeier.asm
+++ solutions/35-Duplicate-Removal-17.167/27.95.specific-StefanLobbenmeier.asm
@@ -1,4 +1,10 @@
 -- HUMAN RESOURCE MACHINE PROGRAM --
+-- 35-Duplicate-Removal - SIZE 27/17 - SPEED 95/167 --
+
+-- This solution is level-specific. It relies on a hashtable of size nine.
+-- Every letter is positioned at [charcode of first letter] - [charcode of current letter]
+-- It assumes that no vowel has a distance to the first vowel of more than 4,
+-- for example first letter E and some letter A, or first letter A and some letter is E
 
     COPYFROM 14
     COPYTO   0
